48e5b0b1d5d9d648c24d54bf3c91ce03fcda2fcc
pangonghang <1966656576@qq.com>
2019-09-22 23:25:54 +0800

    letcode

--- task3.js
+++ task3.js
@@ -1,31 +1,64 @@
-/**
- * @param {number} num
- * @return {number[]}
- */
-var countBits = function(num) {
-    let numberArr = [];
-    // 转二进制
-    /**
-     * @description: 
-     * @param {number} 
-     * @return: number
-     */
-    var bites = function(arg){
-        let result = arg;
-        let resultCount = 0;
-        result = arg.toString(2).split('');
+// // 比特计数
+// /**
+//  * @param {number} num
+//  * @return {number[]}
+//  */
+// var countBits = function(num) {
+//     let numberArr = [];
+//     // 转二进制
+//     /**
+//      * @description: 
+//      * @param {number} 
+//      * @return: number
+//      */
+//     var bites = function(arg){
+//         let result = arg;
+//         let resultCount = 0;
+//         result = arg.toString(2).split('');
         
-        for(let i = 0; i < result.length; i++){
-            if(result[0]=='1') resultCount++;
-        }
+//         for(let i = 0; i < result.length; i++){
+//             if(result[i]=='1') resultCount++;
+//         }
 
-        return resultCount;
-    }
-    //循环
-    for(let i = 0; i <= num; i++){
-        numberArr[i] = bites(i);
-    }
-    return numberArr;
-};
+//         return resultCount;
+//     }
+//     //循环
+//     for(let i = 0; i <= num; i++){
+//         numberArr[i] = bites(i);
+//     }
+//     return numberArr;
+// };
 
-console.log(countBits(1))
+
+// /**
+//  * @param {number} n
+//  * @return {number[][]}
+//  */
+// //螺旋矩阵
+// var generateMatrix = function(n) {
+//     let resArr = [];
+//     for(let i = 0; i < n; i++){
+//         resArr[i] = [];
+//     }
+
+//     let c = 1, j = 0;
+//     while (c <= n * n) {
+//         for (let i = j; i < n - j; i++){
+//                 resArr[j][i] = c++;
+//             }
+//         for (let i = j + 1; i < n - j; i++){
+//                 resArr[i][n - j - 1] = c++;
+//             }
+//         for (let i = n - j - 2; i >= j; i--){
+//                 resArr[n - j - 1][i] = c++;
+//             }
+//         for (let i = n -j - 2; i > j; i--){
+//                 resArr[i][j] = c++;
+//             }
+//         j++;
+//     }
+
+//     return resArr;
+// };
+
+// console.log(generateMatrix(4))

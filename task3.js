1da1949ad058d16ac7d731abf6fd9ab91193bd31
pangonghang <1966656576@qq.com>
2019-09-29 16:45:41 +0800

    new commit

--- task3.js
+++ task3.js
@@ -62,3 +62,102 @@
 // };
 
 // console.log(generateMatrix(4))
+
+// 求子集 问题
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+// var subsets = function(nums) {
+//     let resArr = [[]];
+//     for(let i=0;i<nums.length;i++){
+//         let len = resArr.length;
+//         for(let j=0;j<len;j++){
+//             resArr.push([...resArr[j],nums[i]])
+//         }
+//     }
+//     /* 
+//     task1 resArr = [] [1]
+//     task2 resArr = [] [1]  [2] [1,2]
+//     task3 resArr = [] [1]  [2] [1,2]  [3] [2,3] [1,2,3]
+//      */
+//     return resArr;
+// };
+
+// console.log(subsets([1,2,3]))
+
+/**
+ * @param {number} n
+ * @return {number}
+ * 动态规划问题
+ */
+// var climbStairs = function(n) {
+//     if(n===0||n===1) return 1;
+//     if(n===2) return 2;
+//     let res = [1,2];
+
+//     for(let i = 2; i<n; i++){
+//         res[i] = res[i-1]+res[i-2];
+//     }
+
+//     return res[n-1];
+// };
+
+// console.log(climbStairs(0))
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// var tribonacci = function(n) {
+//     if(n===0) return 0;
+//     if(n===1||n===2) return 1;
+//     let res = [0,1,1];
+    
+//     for(let i = 3; i <= n;i++){
+//         res[i] = res[i-2] + res[i-1] + res[i-3];
+//     }
+
+//     return res[n];
+// }
+
+// console.log(tribonacci(6))
+
+/**
+ * @description: 求数组所有元素的组合
+ * @param {type} {array} nums
+ * @return: array
+ */
+
+ var all = function(nums){
+     let resArr = [];
+     let midllArr = [];
+     /**
+      * @description: 
+      * @param {type} {array} arr
+      * @return: 
+      */
+    let fn = function(arr){
+        if(arr.length===0) return midllArr;
+        let len = arr.length;
+        for(let i = 0; i < len; i++){
+            midllArr.push(arr[i]);
+            let narr = [...arr.slice(0,i)];
+            narr.concat(arr.slice(i))
+            fn(narr);
+        }
+    }
+    fn(nums);
+    resArr.push(fn(nums));
+    console.log(fn(nums))
+    //  nums.forEach((key)=>{
+    //      if(!arr.includes(key)){
+    //          arr.push(num)
+    //      } else {
+
+    //      }
+    //  })
+     return resArr
+ }
+
+ console.log(all([1,2,3]))

696754266fdac1e2f7f30fffd641482caead6535
pangonghang <1966656576@qq.com>
2019-10-26 18:01:55 +0800

    10-26

--- task3.js
+++ task3.js
@@ -853,3 +853,21 @@
     
 //     return head;
 // };
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
+    if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+    return root;
+};

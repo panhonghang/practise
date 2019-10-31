bca104c4bb8360d6bfc0815d271506db2aa67eb1
pangonghang <1966656576@qq.com>
2019-11-01 00:27:20 +0800

    leetcode

--- task4.js
+++ task4.js
@@ -28,15 +28,42 @@
  * @return {number}
  * 26.删除排序数组中的重复项
  */
-var removeDuplicates = function(nums) {
-    let length = nums.length;
-    let res = 1;
+// var removeDuplicates = function(nums) {
+//     let length = nums.length;
+//     let res = 1;
     
-    for(let i = 1; i < length; i++){
-        if(nums[i]!=nums[i-1]){
-           nums[res++] = nums[i];
+//     for(let i = 1; i < length; i++){
+//         if(nums[i]!=nums[i-1]){
+//            nums[res++] = nums[i];
+//         }
+//     }
+
+//     return res;
+// }; 
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ * 最长公共前缀
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length < 1) return '';
+
+    let res = [];
+    let len = strs[0].length;
+
+    for(let i = 0; i < len; i++){
+        let fn = (key)=>{
+            return key[i] == strs[0][i];
+        }
+        if(strs.every(fn)){
+            res.push(strs[0][i]);
+        } else {
+            break;
         }
     }
+    
+    return res.join('');
+};
 
-    return res;
-}; 
+console.log(longestCommonPrefix(["ower","ow","light"]))

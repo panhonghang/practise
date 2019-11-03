b40d070bfcd78e95cd1f700f003c6ae262e3d3cc
pangonghang <1966656576@qq.com>
2019-11-03 21:37:01 +0800

    leetcode

--- task4.js
+++ task4.js
@@ -91,40 +91,51 @@
  * @return {string}
  * 最长回文字符
  */
-var longestPalindrome = function(s) {
+// var longestPalindrome = function(s) {
 
-    if (!s || !s.trim()) return '';
-    if (s.length === 1) return s;
-    if (s.length === 2) return s[0] === s[1] ? s[0] + s[1] : s[1];
-    let result = '';
+//     if (!s || !s.trim()) return '';
+//     if (s.length === 1) return s;
+//     if (s.length === 2) return s[0] === s[1] ? s[0] + s[1] : s[1];
+//     let result = '';
 
-    /**
-    *扩散坐标
-    */
-    var calPalindromeIndex = function(left,right,s){
-        let len = s.length;
-        while(left>=0&&right<len&&s[left] == s[right]){
-            left--;
-            right++;
-        }
-        return {left:left+1,right:right}
-    }
+//     /**
+//     *扩散坐标
+//     */
+//     var calPalindromeIndex = function(left,right,s){
+//         let len = s.length;
+//         while(left>=0&&right<len&&s[left] == s[right]){
+//             left--;
+//             right++;
+//         }
+//         return {left:left+1,right:right}
+//     }
 
 
-    for(let i = 0,len = s.length;i<len;i++){
-        let even = '';
-        let odd = '';
-        if(s[i] == s[i+1]){
-            //经过当前位与下一位判断已构成回文，扩散位直接从下一位开始，可以提速
-            let evenIndex = calPalindromeIndex(i-1,i+2,s);
-            even = s.slice(evenIndex.left,evenIndex.right);
-        }
+//     for(let i = 0,len = s.length;i<len;i++){
+//         let even = '';
+//         let odd = '';
+//         if(s[i] == s[i+1]){
+//             //经过当前位与下一位判断已构成回文，扩散位直接从下一位开始，可以提速
+//             let evenIndex = calPalindromeIndex(i-1,i+2,s);
+//             even = s.slice(evenIndex.left,evenIndex.right);
+//         }
 
-        let oddIndex = calPalindromeIndex(i-1,i+1,s);
-        odd = s.slice(oddIndex.left,oddIndex.right);
+//         let oddIndex = calPalindromeIndex(i-1,i+1,s);
+//         odd = s.slice(oddIndex.left,oddIndex.right);
         
-        let re = odd.length>even.length?odd:even;
-        result = result.length>re.length?result:re;
-    }
-    return result;
-};
+//         let re = odd.length>even.length?odd:even;
+//         result = result.length>re.length?result:re;
+//     }
+//     return result;
+// };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ * 存在重复元素
+ */
+// var containsDuplicate = function(nums) {
+//     let arr = [...new Set(nums)];
+//     return nums.join('') === arr.join('');
+// };
+

25361af0b273b7b41bf0f4b47fb739026c93bc00
pangonghang <1966656576@qq.com>
2019-11-14 23:08:59 +0800

    leetcode

--- task4.js
+++ task4.js
@@ -821,3 +821,22 @@
 //     }
 //     return dp[n-1][m-1];
 // };
+
+/**
+ * @param {number} n
+ * @return {number}
+ * 70.爬楼梯
+ */
+var climbStairs = function(n) {
+    let arr  = [];
+    arr[0] = 1;
+    arr[1] = 1;
+
+    for(let i = 2; i <= n; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    return arr[n];
+};
+
+climbStairs(4)

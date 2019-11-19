929e19dcfa2c54d41be4d7cf37b62c78fca13cc9
pangonghang <1966656576@qq.com>
2019-11-20 01:04:17 +0800

    leetcode

--- task4.js
+++ task4.js
@@ -977,3 +977,32 @@ var divisorGame = function(N) {
 var stoneGame = function(piles) {
     return true;
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ * 96. 不同的二叉搜索树
+ */
+
+//  状态方程 当n>3的时候 dp[n] = 2*(dp[0] +。。。。。+ dp[n-1])
+
+var numTrees = function(n) {
+    if(n<=1) return 1;
+    if(n==2) return 2;
+
+    let dp = new Array(n+1);
+        dp.fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 5;
+
+    for(let i = 4; i <= n; i++){
+        for(let j = 0; j < i; j++){
+            dp[i] += dp[j]*dp[i-j-1]
+        }
+    }
+    return dp[n]
+};
+
+numTrees(10);

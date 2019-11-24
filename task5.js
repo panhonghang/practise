ba429265c987209a1d0d10771468c4247c761a15
pangonghang <1966656576@qq.com>
2019-11-25 00:31:24 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -4,23 +4,14 @@
  * 647. 回文子串
  */
 var countSubstrings = function(s) {
-    let temp = [[]],
-        len = s.length,
+    let len = s.length,
         res = 0;
     if(len == 0) return 0;
     for(let i = 0; i < len; i++){
-        let len1 = temp.length;
-        for(let j = 0; j < len1; j++){
-            temp.push([...temp[j],s[i]])
+        for(let j = i + 1; j <= len; j++){
+           if(s.substring(i,j) == s.substring(i,j).split('').reverse().join('')) res++
         }
     }
-    temp.shift();
 
-    for(let i = 0; i < temp.length; i++){
-        if(temp[i].join('') == temp[i].reverse().join('')) res++;
-    }
-    console.log(temp)
     return res;
 };
-
-countSubstrings("abc")

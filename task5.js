f89716a931f2f238755546290e6510ca6da873eb
pangonghang <1966656576@qq.com>
2019-11-26 21:42:05 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -116,3 +116,23 @@ var minDistance = function(word1, word2) {
     }
     return dp[len1][len2]
 };
+
+// 更优解法
+var minDistance = function (word1, word2) {
+	let m = word1.length
+	let n = word2.length
+	let dp = []
+	for (let i = 0; i <= m; i++) {
+		dp.push([])
+		for (let j = 0; j <= n; j++) {
+			if (i * j) {
+				dp[i][j] = word1[i - 1] === word2[j - 1]
+					? dp[i - 1][j - 1]
+					: 1 + Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
+			} else {
+				dp[i][j] = i + j
+			}
+		}
+	}
+	return dp[m][n]
+};

5039ebfb04d60c0f3e40968ff56678ca6bb1ae45
pangonghang <1966656576@qq.com>
2019-11-26 22:51:14 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -237,7 +237,22 @@ var isHappy = function(n) {
     return true;
   };
 
- 
+  /**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ *   两整数之和
+ */
+var getSum = function(a, b) {
+    while (b !== 0) {
+        let temp = a ^ b;
+        b = (a & b) << 1;
+        a = temp;
+    }
+    return a;
+};
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 

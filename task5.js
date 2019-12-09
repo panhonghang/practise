00e55ed288a4255f550bd56a3e56d19690184b5d
pangonghang <1966656576@qq.com>
2019-12-09 23:15:40 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -629,3 +629,36 @@ console.log(gameOfLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]))
 var toLowerCase = function(str) {
   return str.toLocaleLowerCase();
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ * 24. 两两交换链表中的节点
+ */
+var swapPairs = function(head) {
+  let pre = new ListNode(0);
+   
+  pre.next = head;
+
+  let temp = pre;
+
+  while(temp.next != null && temp.next.next != null) {
+       
+      let start = temp.next,
+          end = temp.next.next;
+
+      temp.next = end;
+      start.next = end.next;
+       
+      end.next = start;
+      temp = start;
+  }
+  return pre.next;
+};

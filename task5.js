dc55cd54f3a47b7d1cea71b13b1552a84ef7a5bb
pangonghang <1966656576@qq.com>
2019-12-12 21:13:14 +0800

    leetcode、

--- task5.js
+++ task5.js
@@ -726,3 +726,24 @@ function Student(name, age){
 let task1 = newfn(Student,'aa',11);
 
 console.log(task1)
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  if(needle.length==0) return 0;
+  return haystack.search(needle)
+};
+
+var strStr = function (haystack, needle) {
+  if (needle === "") return 0
+  for (var i = 0; i < haystack.length; i++) {
+      if (haystack[i] === needle[0]) {
+          if (haystack.substring(i, i + needle.length) === needle) return i;
+      }
+  }
+  return -1
+};
+

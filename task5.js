f1e7cc0d0984143fdc11870045ba4c5a544a8a00
pangonghang <1966656576@qq.com>
2019-12-14 23:29:59 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -841,3 +841,32 @@ let c = {
 }
 
 let a2 = a.bind1(b,'aa')();
+
+
+/* JavaScript实现常见排序算法 */
+
+/* 
+  稳定：如果a原本在b前面，而a=b，排序之后a仍然在b的前面；
+  不稳定：如果a原本在b的前面，而a=b，排序之后a可能会出现在b的后面；
+  内排序：所有排序操作都在内存中完成；
+  外排序：由于数据太大，因此把数据放在磁盘中，而排序通过磁盘和内存的数据传输才能进行；
+  时间复杂度: 一个算法执行所耗费的时间。
+  空间复杂度: 运行完一个程序所需内存的大小。
+  链接：https://user-gold-cdn.xitu.io/2016/11/29/4abde1748817d7f35f2bf8b6a058aa40?imageView2/0/w/1280/h/960/format/webp/ignore-error/1
+ */
+
+// 冒泡排序
+function bubbleSort(arr) {
+  let len = arr.length;
+  for(let i=0; i<len; i++) {
+      for(let j=0; j<len - 1 - i; j++) {
+          if(arr[j] > arr[j+1]) {
+              [arr[j+1], arr[j]] = [arr[j], arr[j+1]];
+          }
+      }
+  }
+  return arr;
+}
+
+// 选择排序
+

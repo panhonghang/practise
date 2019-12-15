40f7e7d4761feaac5866c68464f0118a13064c14
pangonghang <1966656576@qq.com>
2019-12-15 23:44:54 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -870,3 +870,19 @@ function bubbleSort(arr) {
 
 // 选择排序
 
+function selectionSort(arr) {
+  var len = arr.length;
+  var minIndex;
+  for (var i = 0; i < len - 1; i++) {
+      minIndex = i;
+      for (var j = i + 1; j < len; j++) {
+          if (arr[j] < arr[minIndex]) {     //寻找最小的数
+              minIndex = j;                 //将最小数的索引保存
+          }
+      }
+      [arr[i],arr[minIndex]] = [arr[minIndex],arr[i]]
+  }
+  return arr;
+}
+var arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
+console.log(selectionSort(arr));//[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]

eb331e5aa0e9ecbc0bbb4381eca9e2c0f028b90c
pangonghang <1966656576@qq.com>
2019-12-18 22:59:26 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -973,9 +973,24 @@ function mergeSort(arr){
     return merge(mergeSort(left),mergeSort(right))
 }
 
-
-
-
-
-
-
+/* 希尔排序 */
+function shellSort(arr) {
+    var len = arr.length,
+        temp,
+        gap = 1;
+    console.time('希尔排序耗时:');
+    while(gap < len/5) {          //动态定义间隔序列
+        gap =gap*5+1;
+    }
+    for (gap; gap > 0; gap = Math.floor(gap/5)) {
+        for (var i = gap; i < len; i++) {
+            temp = arr[i];
+            for (var j = i-gap; j >= 0 && arr[j] > temp; j-=gap) {
+                arr[j+gap] = arr[j];
+            }
+            arr[j+gap] = temp;
+        }
+    }
+    console.timeEnd('希尔排序耗时:');
+    return arr;
+}

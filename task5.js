f3e757d4670a0739c19ab9aec79552833ffe86af
pangonghang <1966656576@qq.com>
2019-12-19 23:58:13 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -994,3 +994,46 @@ function shellSort(arr) {
     console.timeEnd('希尔排序耗时:');
     return arr;
 }
+
+/* 堆排序 */
+
+// 交换两个节点
+function swap(A, i, j) {
+    let temp = A[i];
+    A[i] = A[j];
+    A[j] = temp; 
+  }
+  //顶堆
+  function shiftDown(A, i, length) {
+    let temp = A[i]; // 当前父节点
+  // j<length 的目的是对结点 i 以下的结点全部做顺序调整
+    for(let j = 2*i+1; j<length; j = 2*j+1) {
+      temp = A[i];  // 将 A[i] 取出，整个过程相当于找到 A[i] 应处于的位置
+      if(j+1 < length && A[j] < A[j+1]) { 
+        j++;   // 找到两个孩子中较大的一个，再与父节点比较
+      }
+      if(temp < A[j]) {
+        swap(A, i, j) // 如果父节点小于子节点:交换；否则跳出
+        i = j;  // 交换后，temp 的下标变为 j
+      } else {
+        break;
+      }
+    }
+  }
+  
+  // 堆排序
+  function heapSort(A) {
+    // 初始化大顶堆，从第一个非叶子结点开始
+    for(let i = Math.floor(A.length/2-1); i>=0; i--) {
+      shiftDown(A, i, A.length);
+    }
+    // 排序，每一次for循环找出一个当前最大值，数组长度减一
+    for(let i = Math.floor(A.length-1); i>0; i--) {
+      swap(A, 0, i); // 根节点与最后一个节点交换
+      shiftDown(A, 0, i); 
+    }
+  }
+  
+  let Arr = [4, 6, 8, 5, 9, 1, 2, 5, 3, 2];
+  heapSort(Arr);
+  alert(Arr);

5106fe3eeb00a0c18fc5829153adb87dccfbb349
pangonghang <1966656576@qq.com>
2020-02-15 20:37:35 +0800

    剑指offer25题

--- task5.js
+++ task5.js
@@ -1598,3 +1598,35 @@ var singleNumber = function(nums) {
     }
     return nums[pre]
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ * @description 剑指offer 25题
+ */
+var mergeTwoLists = function(l1, l2) {
+    let head , temp;
+    head = temp = new ListNode(0);
+
+    while(l1&&l2){
+        if(l1.val>l2.val){
+            temp.next = new ListNode(l2.val);
+            l2 = l2.next;
+        } else {
+            temp.next = new ListNode(l1.val);
+            l1 = l1.next;
+        }
+        temp = temp.next;
+    }
+    temp.next = l1?l1:l2;
+    
+    return head.next;
+};

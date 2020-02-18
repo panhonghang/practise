682b59ccd17cdba54aff824e8f1d52d4cb2944f9
pangonghang <1966656576@qq.com>
2020-02-18 21:40:53 +0800

    剑指offer68题

--- task5.js
+++ task5.js
@@ -1853,3 +1853,29 @@ var findRepeatNumber = function(nums) {
     }
     return -1
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ * 面试题68 - I. 二叉搜索树的最近公共祖先
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(root.val < p.val&& root.val < q.val ){
+       return lowestCommonAncestor(root.right, p, q)
+    } else if(root.val > p.val&& root.val > q.val ){
+       return lowestCommonAncestor(root.left, p, q)
+    } else if((root.val > p.val&& root.val < q.val)||(root.val < p.val&& root.val > q.val )){
+        return root
+    } else {
+        return root.val==q.val?q:p
+    }
+};

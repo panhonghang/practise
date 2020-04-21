a058edd04a2c3971acb0912e34b0ad3ae9baae5e
pangonghang <1966656576@qq.com>
2020-04-21 21:26:45 +0800

    平时

--- task5.js
+++ task5.js
@@ -3800,13 +3800,41 @@
 // console.log(a)
 
 
+/*
+* @param {number[]} nums
+* @param {number} k
+* @return {number}
+*/
 
+var numberOfSubarrays = function(nums, k) {
+ let len = nums.length,
+     oddArr = []                    // 存储奇数 索引
 
+ for(let i = 0; i < len; i++){   // 找出所有奇数的索引
+   if(nums[i] % 2 === 1){
+     oddArr.push(i)
+   }
+ }
 
-
-
-
-
-
-
-
+ if(oddArr.length < k) return 0                 // 当奇数的个数小于k时 直接返回, 其实不用这段也可以,下面根本不会进循环
+ 
+ let total = 0                               // 接下来就是找出k位奇数两边有多少个偶数了, 计总数
+ for(let i = 0; i <= oddArr.length - k; i++){   // 循环奇数索引数组, 找出左右两边偶数
+   
+  let left = 0, right = 0                                   // 用来统计左右两边偶数
+  let left_index = oddArr[i], right_index = oddArr[i + k - 1]     // 开始位置
+   
+   while(left_index > 0 && nums[left_index - 1] % 2 === 0){  // 往左找到不是偶数的为止
+     left_index--
+     left++
+   }
+   
+   while(right_index < len && nums[right_index + 1] % 2 === 0){ // 往右找到不是偶数的为止
+     right++
+     right_index++
+   }
+   
+   total += (left + 1) * (right + 1)
+ }
+ return total
+};

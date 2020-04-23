6a6562fc1c6823f8ac59ae36af2b386e1f8a402d
pangonghang <1966656576@qq.com>
2020-04-23 22:59:48 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -3806,35 +3806,71 @@
 * @return {number}
 */
 
-var numberOfSubarrays = function(nums, k) {
- let len = nums.length,
-     oddArr = []                    // 存储奇数 索引
+// var numberOfSubarrays = function(nums, k) {
+//  let len = nums.length,
+//      oddArr = []                    // 存储奇数 索引
 
- for(let i = 0; i < len; i++){   // 找出所有奇数的索引
-   if(nums[i] % 2 === 1){
-     oddArr.push(i)
-   }
- }
+//  for(let i = 0; i < len; i++){   // 找出所有奇数的索引
+//    if(nums[i] % 2 === 1){
+//      oddArr.push(i)
+//    }
+//  }
 
- if(oddArr.length < k) return 0                 // 当奇数的个数小于k时 直接返回, 其实不用这段也可以,下面根本不会进循环
+//  if(oddArr.length < k) return 0                 // 当奇数的个数小于k时 直接返回, 其实不用这段也可以,下面根本不会进循环
  
- let total = 0                               // 接下来就是找出k位奇数两边有多少个偶数了, 计总数
- for(let i = 0; i <= oddArr.length - k; i++){   // 循环奇数索引数组, 找出左右两边偶数
+//  let total = 0                               // 接下来就是找出k位奇数两边有多少个偶数了, 计总数
+//  for(let i = 0; i <= oddArr.length - k; i++){   // 循环奇数索引数组, 找出左右两边偶数
    
-  let left = 0, right = 0                                   // 用来统计左右两边偶数
-  let left_index = oddArr[i], right_index = oddArr[i + k - 1]     // 开始位置
+//   let left = 0, right = 0                                   // 用来统计左右两边偶数
+//   let left_index = oddArr[i], right_index = oddArr[i + k - 1]     // 开始位置
    
-   while(left_index > 0 && nums[left_index - 1] % 2 === 0){  // 往左找到不是偶数的为止
-     left_index--
-     left++
-   }
+//    while(left_index > 0 && nums[left_index - 1] % 2 === 0){  // 往左找到不是偶数的为止
+//      left_index--
+//      left++
+//    }
    
-   while(right_index < len && nums[right_index + 1] % 2 === 0){ // 往右找到不是偶数的为止
-     right++
-     right_index++
-   }
+//    while(right_index < len && nums[right_index + 1] % 2 === 0){ // 往右找到不是偶数的为止
+//      right++
+//      right_index++
+//    }
    
-   total += (left + 1) * (right + 1)
- }
- return total
-};
+//    total += (left + 1) * (right + 1)
+//  }
+//  return total
+// };
+
+// let waysToChange = (n)=> {
+//   let dp = new Array(n+1).fill(1)
+//   let coins = [1,5,10,25];
+
+//   for(let i=1; i<4; i++){
+//       for(let j=1; j<=n; j++){
+//           if(j-coins[i]>=0){
+//               dp[j] = (dp[j]+dp[j-coins[i]]) % (1e9+7)
+//           } 
+//       }
+//   }
+//   return dp[n]
+// };
+
+let waysToChange = (n)=>{
+  let arr = new Array(n+1).fill(0),
+      dp= new Array(4).fill(arr),
+      coins= [1,5,10,25];
+
+  for(let i=0;i<=n;i++) dp[0][i]=1;
+  for(let i=0;i<4;i++) dp[i][0]=1;
+      
+  for(let i=1;i<4;i++){
+      for(let j=1;j<=n;j++){
+          if(j>=coins[i]){
+            dp[i][j]=(dp[i-1][j]+dp[i][j-coins[i]])%1000000007;
+          } else {
+            dp[i][j]=dp[i-1][j];
+          }
+      }
+  }
+  return dp[3][n];
+}
+
+console.log(waysToChange(100));

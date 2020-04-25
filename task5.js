8272aceb220b1edfe7bbe68c4f1167ac0b5321ba
pangonghang <1966656576@qq.com>
2020-04-25 19:58:18 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -3926,3 +3926,20 @@
 // };
 
 // console.log(permute([1,2,3]))
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  const nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+        roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  let res = "";
+  for (let i = 0; i < nums.length; i++) {
+      while (num >= nums[i]) {
+          num -= nums[i];
+          res += roman[i];
+      }
+  }
+  return res;
+};

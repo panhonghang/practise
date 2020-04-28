7c699a6deb7172e635a631008502d347a38b2359
pangonghang <1966656576@qq.com>
2020-04-28 20:51:18 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -4076,32 +4076,66 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-  let left = 0;
- let right = nums.length - 1;
- while (left <= right) {
-     let mid = left + Math.floor((right - left) / 2)
-     if (target === nums[mid]) {
-         return mid;
-     }
-     // 目标值在左升序区间逻辑
-     if (nums[left] <= nums[mid]) {
-         // 位于区间内，从右逼近
-         if (target >= nums[left] && target <= nums[mid]) {
-             right = mid - 1;
-         } else {
-             // 不位于区间内，从左逼近
-             left = mid + 1;
-         }
-     } else { // 目标值在右升序区间逻辑
-         // 位于区间内，从左逼近
-         if (target >= nums[mid] && target <= nums[right]) {
-             left = mid + 1
-         } else {
-             // 不位于区间内，从右逼近
-             right = mid - 1;
-         }
-     }
- }
- return -1;
+// var search = function(nums, target) {
+//   let left = 0;
+//  let right = nums.length - 1;
+//  while (left <= right) {
+//      let mid = left + Math.floor((right - left) / 2)
+//      if (target === nums[mid]) {
+//          return mid;
+//      }
+//      // 目标值在左升序区间逻辑
+//      if (nums[left] <= nums[mid]) {
+//          // 位于区间内，从右逼近
+//          if (target >= nums[left] && target <= nums[mid]) {
+//              right = mid - 1;
+//          } else {
+//              // 不位于区间内，从左逼近
+//              left = mid + 1;
+//          }
+//      } else { // 目标值在右升序区间逻辑
+//          // 位于区间内，从左逼近
+//          if (target >= nums[mid] && target <= nums[right]) {
+//              left = mid + 1
+//          } else {
+//              // 不位于区间内，从右逼近
+//              right = mid - 1;
+//          }
+//      }
+//  }
+//  return -1;
+// };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumbers = function(nums) {
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    nums.forEach((item) => {
+    c ^= item;
+    });
+    let mark = 1;             // mark就是分组凭据
+    console.log(mark&c)
+    while((mark & c) === 0) { // 一直到找到第一个1为止 
+        mark <<= 1;
+    }
+    console.log(mark)
+    nums.forEach((item) => {
+    if ((mark & item) === 0) { // 分组
+        a ^= item
+    } else {
+        b ^= item;
+    }
+    });
+    return [a, b];
 };
+
+singleNumbers([3,4,2,3])

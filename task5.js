076204b33f4d7297124b7f0e711f108c81eb0486
pangonghang <1966656576@qq.com>
2020-04-29 19:30:03 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -4154,3 +4154,12 @@ var replaceSpaces = function(S, length) {
       }
   }).slice(0,length).join('');
 };
+
+/**
+ * @param {string} S
+ * @param {number} length
+ * @return {string}
+ */
+var replaceSpaces = function(S, length) {
+  return S.slice(0,length).replace(/[ ]/g,'%20');
+};

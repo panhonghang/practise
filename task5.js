b83d30faf3fc03dfaeb54572d986187875e052b2
pangonghang <1966656576@qq.com>
2020-05-03 20:27:30 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -4430,3 +4430,43 @@ var maxSubArray = function(nums) {
     })
     return res;
 };
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const map = {
+        I : 1,
+        IV: 4,
+        V: 5,
+        IX: 9,
+        X: 10,
+        XL: 40,
+        L: 50,
+        XC: 90,
+        C: 100,
+        CD: 400,
+        D: 500,
+        CM: 900,
+        M: 1000
+    };
+    let ans = 0,
+        len = s.length,
+        i = 0;
+    while(i < len) {
+        if(i == len-1) {
+            ans += map[s[i]];
+            break;
+        }
+        if(map[s[i]+s[i+1]]) {
+            ans += map[s[i]+s[i+1]];
+            i += 2;
+        } else {
+            ans += map[s[i]];
+            i++;
+        }
+    }
+    return ans;
+};

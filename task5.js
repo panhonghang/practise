f840f19db40ac3e2efc5aea4f77fa8839ada9e07
pangonghang <1966656576@qq.com>
2020-05-09 20:10:23 +0800

    leetcode

--- task5.js
+++ task5.js
@@ -4609,3 +4609,25 @@ var isSubtree = function(s, t) {
     fn(s);    
     return flag
 };
+
+var mySqrt = function(x) {
+    if (x < 2) {
+        return x;
+    }
+    let left = 2;
+    let right = x;
+    while (left <= right) {
+        let mid = left + Math.floor((right - left) / 2);
+        let num = mid * mid;
+        if (num > x) {
+            right = mid - 1;
+        }
+        else if (num < x) {
+            left = mid + 1;
+        }
+        else {
+            return mid;
+        }
+    }
+    return right;
+};

bdbd395f6193886e75a5d18003b95076d2a96f33
pangonghang <1966656576@qq.com>
2020-05-23 19:54:25 +0800

    剪绳子

--- task5.js
+++ task5.js
@@ -5578,3 +5578,19 @@
 
 //     return head.next;
 // };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// var cuttingRope = function(n) {
+//     let dp = new Array(n+1).fill(1);
+    
+//     for(let i = 2; i < n+1; i++){
+//         for(let j = 1; j < i-1; j++){
+//             dp[i] = Math.max(dp[i],Math.max(dp[i-j]*j,j*(i-j)));
+//         }
+//     }
+
+//     return dp[n];
+// };

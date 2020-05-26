00ef6dc53f40660dfaa73e825c574cc78ab0d0a9
pangonghang <1966656576@qq.com>
2020-05-26 17:37:51 +0800

    贪心解决分饼干

--- task5.js
+++ task5.js
@@ -5873,3 +5873,27 @@
 
 //     return C;
 // };
+
+// /**
+//  * @param {number[]} g
+//  * @param {number[]} s
+//  * @return {number}
+//  */
+// var findContentChildren = function(g, s) {
+//     g.sort((a,b)=>a-b);
+//     s.sort((a,b)=>a-b);
+
+//     let res = 0,
+//         i = 0,
+//         j = 0;
+
+//     while (i < g.length && j < s.length){
+//         if(s[j] >= g[i]){
+//             res++;
+//             i++;
+//         }
+//         j++;
+//     }
+
+//     return res;
+// };

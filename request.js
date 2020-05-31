0a45c31c0467655baedbd72760dde399cff61333
pangonghang <1966656576@qq.com>
2020-05-31 10:17:18 +0800

    教务在线

--- request.js
+++ request.js
@@ -1,26 +1,26 @@
-var request = require('request');
-var fs = require('fs');
+// var request = require('request');
+// var fs = require('fs');
 
-let num = 200;
+// let num = 200;
 
-while(num<201){
-    header={
-        "Cookie":"PHPSESSID=ST-104038-Kmce2aotmkZklgbWmYf7-fI3L-ids1-1590836340029",
-    }
+// while(num<201){
+//     header={
+//         "Cookie":"PHPSESSID=ST-104038-Kmce2aotmkZklgbWmYf7-fI3L-ids1-1590836340029",
+//     }
     
-    const options = {
-        url: 'http://jwc.cqupt.edu.cn/showstupic.php?xh=2017210016'+num,
-        headers: header,
-    };
+//     const options = {
+//         url: 'http://jwc.cqupt.edu.cn/showstupic.php?xh=2017210016'+num,
+//         headers: header,
+//     };
     
-    function downloadFile(filename,callback){
-        var stream = fs.createWriteStream(filename);
-        request(options).pipe(stream).on('close', callback); 
-    }
+//     function downloadFile(filename,callback){
+//         var stream = fs.createWriteStream(filename);
+//         request(options).pipe(stream).on('close', callback); 
+//     }
 
-    var filename = '2018210'+num+'.jpg';
-    downloadFile(filename,function(a,b,c){
-        console.log(a,b,c);
-    });
-    num++;
-}
+//     var filename = '2018210'+num+'.jpg';
+//     downloadFile(filename,function(a,b,c){
+//         console.log(a,b,c);
+//     });
+//     num++;
+// }

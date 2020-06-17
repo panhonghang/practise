45c3b067c23a8093ca991ec1617958835e9617d4
pangonghang <1966656576@qq.com>
2020-06-18 02:02:12 +0800

    从先序遍历还原二叉树

--- task5.js
+++ task5.js
@@ -7568,4 +7568,25 @@ var recoverFromPreorder = function(S) {
   return root;
 };
 
+var recoverFromPreorder = function(S) {
+  const parentStack = [new TreeNode(0)];
+  for (let i = 0, curDepth = 0, num = '', len = S.length; i < len; ++i) {
+    if (S[i] === '-') {
+      ++curDepth
+    } else {
+      while(i < len && S[i] !== '-') num += S[i++];
+      while (parentStack.length - curDepth !== 1) parentStack.pop();
+      const parent = parentStack[parentStack.length - 1];
+      const node = new TreeNode(num);
+      if (!parent.left) parent.left = node
+      else parent.right = node
+      parentStack.push(node);
+      num = '';
+      curDepth = 1;
+    }
+  }
+  return parentStack[0].left;
+};
+
+
 recoverFromPreorder("100-401--349---90--88")

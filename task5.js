57376cf723bbece73899411148567004bb5a8316
pangonghang <1966656576@qq.com>
2020-06-27 21:43:34 +0800

    原地哈希

--- task5.js
+++ task5.js
@@ -7922,3 +7922,23 @@
 //   }
 //   return head;
 // };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+  let len = nums.length;
+
+  for(let i = 0; i < len; i++){
+    while(nums[i]>0&&nums[i]<len&&nums[nums[i]-1]!=nums[i]){
+      [nums[nums[i]-1],nums[i]] = [nums[i],nums[nums[i]-1]];
+    }
+  }
+
+  for(let i = 0; i < len; i++){
+    if(nums[i]!==i+1) return i+1;
+  }
+  
+  return len+1;
+};

bdb0e993ab03fb7083f182c57a0bf5bb97755bfe
pangonghang <1966656576@qq.com>
2020-06-27 21:44:50 +0800

    原地哈希

--- task5.js
+++ task5.js
@@ -7927,18 +7927,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive = function(nums) {
-  let len = nums.length;
+// var firstMissingPositive = function(nums) {
+//   let len = nums.length;
 
-  for(let i = 0; i < len; i++){
-    while(nums[i]>0&&nums[i]<len&&nums[nums[i]-1]!=nums[i]){
-      [nums[nums[i]-1],nums[i]] = [nums[i],nums[nums[i]-1]];
-    }
-  }
+//   for(let i = 0; i < len; i++){
+//     while(nums[i]>0&&nums[i]<len&&nums[nums[i]-1]!=nums[i]){
+//       [nums[nums[i]-1],nums[i]] = [nums[i],nums[nums[i]-1]];
+//     }
+//   }
 
-  for(let i = 0; i < len; i++){
-    if(nums[i]!==i+1) return i+1;
-  }
-  
-  return len+1;
-};
+//   for(let i = 0; i < len; i++){
+//     if(nums[i]!==i+1) return i+1;
+//   }
+
+//   return len+1;
+// };

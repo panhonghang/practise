d0b0d131c7e732ca7069c4355387f0fd4d261e46
pangonghang <1966656576@qq.com>
2020-06-29 00:37:28 +0800

    第K大

--- task5.js
+++ task5.js
@@ -7966,3 +7966,14 @@
 
 //   return res === Infinity?0:res;
 // };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+// var findKthLargest = function(nums, k) {
+//     let arr = nums.sort((a,b)=>b-a);
+//     if(k > arr.length) return arr.pop();
+//     return arr[k-1];
+// };

639379a0361a34330607c365ce314a03882eca19
pangonghang <1966656576@qq.com>
2020-06-30 22:10:59 +0800

    栈实现队列

--- task5.js
+++ task5.js
@@ -7978,25 +7978,25 @@
 //     return arr[k-1];
 // };
 
-var CQueue = function() {
-    this.arr = [];
-};
+// var CQueue = function() {
+//     this.arr = [];
+// };
 
-/** 
- * @param {number} value
- * @return {void}
- */
-CQueue.prototype.appendTail = function(value) {
-    this.arr.push(value);
-};
+// /** 
+//  * @param {number} value
+//  * @return {void}
+//  */
+// CQueue.prototype.appendTail = function(value) {
+//     this.arr.push(value);
+// };
 
-/**
- * @return {number}
- */
-CQueue.prototype.deleteHead = function() {
-    if(this.arr.length===0) return -1;
-    return this.arr.shift();
-};
+// /**
+//  * @return {number}
+//  */
+// CQueue.prototype.deleteHead = function() {
+//     if(this.arr.length===0) return -1;
+//     return this.arr.shift();
+// };
 
 /**
  * Your CQueue object will be instantiated and called as such:

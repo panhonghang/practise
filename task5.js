6febaa97349217bae04a7bb69b80b99974be291b
pangonghang <1966656576@qq.com>
2020-07-04 20:04:47 +0800

    中序

--- task5.js
+++ task5.js
@@ -8027,4 +8027,17 @@
 //         }
 //     }
 //     return res;
-// };;
+// };
+
+
+// var sortedArrayToBST = function(nums) {
+//     // 由于数组是排序好的，因此一个思路就是将数组分成两半，一半是左子树，另一半是右子树
+//     // 然后运用“树的递归性质”递归完成操作即可。
+//     if(nums.length === 0) return null;
+//     const mid = nums.length >> 1;
+//     const root = new  TreeNode(nums[mid]);
+
+//     root.left = sortedArrayToBST(nums.slice(0, mid));
+//     root.right = sortedArrayToBST(nums.slice(mid + 1))
+//     return root;
+// };

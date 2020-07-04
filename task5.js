3bc81f2429c241f722f996ba5e542d607d33735b
pangonghang <1966656576@qq.com>
2020-07-04 23:44:44 +0800

    最长有效括号

--- task5.js
+++ task5.js
@@ -8045,21 +8045,21 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function(s) {
-    let res = 0,
-        stack = [-1];
+// var longestValidParentheses = function(s) {
+//     let res = 0,
+//         stack = [-1];
   
-    for(let i = 0;i < s.length;i++){  
-      if(s[i] == '('){
-        stack.push(i);
-      }else{
-        stack.pop();
-        if(stack.length == 0){
-          stack.push(i);
-        }else{
-          res = Math.max(res,i - stack[stack.length-1]);
-        }
-      }
-    }
-    return res;
-  };
+//     for(let i = 0;i < s.length;i++){  
+//       if(s[i] == '('){
+//         stack.push(i);
+//       }else{
+//         stack.pop();
+//         if(stack.length == 0){
+//           stack.push(i);
+//         }else{
+//           res = Math.max(res,i - stack[stack.length-1]);
+//         }
+//       }
+//     }
+//     return res;
+//   };

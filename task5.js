2fe287e5fce989ba819cfbe0558c1ccf33205dfa
pangonghang <1966656576@qq.com>
2020-07-06 22:20:55 +0800

    正则

--- task5.js
+++ task5.js
@@ -8063,3 +8063,29 @@
 //     }
 //     return res;
 //   };
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+  if (p === "*" || s === p) return true;
+  let dp = Array.from(Array(s.length + 1), ()=> Array(p.length + 1).fill(false));
+  
+  dp[0][0] = true;
+  for (let i  = 1; i <= p.length; i++) {
+      if (!dp[0][i - 1]) break;
+      if (p[i - 1] === '*') dp[0][i] = true;
+  }
+  for (let i = 1; i <= s.length; i++) {
+      for (let j = 1; j <= p.length; j++) {
+          if (s[i - 1] === p[j - 1] || p[j - 1] === "?") {
+              dp[i][j] = dp[i - 1][j - 1];
+          } else if (p[j - 1] === "*") {
+              dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+          }
+      }
+  }
+  return dp[s.length][p.length];
+};

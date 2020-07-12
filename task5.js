fc2473ad8e4bb4dee9641cbac7198fb28577e663
honghang.phh <honghang.phh.@alibaba-inc.com>
2020-07-12 23:28:21 +0800

    地下城

--- task5.js
+++ task5.js
@@ -8258,3 +8258,27 @@
 //     if (arr[lo] < target) return lo + 1; // 目标值比lo元素大，lo还需+1
 //     return lo;    // 否则 返回lo
 //   };
+
+/**
+ * @param {number[][]} dungeon
+ * @return {number}
+ */
+var calculateMinimumHP = function(dungeon) {
+    let n = dungeon.length, 
+        m = dungeon[0].length,
+        dp = [];
+
+    for (let i = 0; i <= n; ++i) {
+        dp.push(new Array(n+1).fill(Number.MAX_SAFE_INTEGER));
+    }
+
+    dp[n][m - 1] = dp[n - 1][m] = 1;
+    
+    for (let i = n - 1; i >= 0; --i) {
+        for (let j = m - 1; j >= 0; --j) {
+            let minn = Math.min(dp[i + 1][j], dp[i][j + 1]);
+            dp[i][j] = Math.max(minn - dungeon[i][j], 1);
+        }
+    }
+    return dp[0][0];
+};

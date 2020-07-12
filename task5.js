891c2f5a6c413befdcb90d6b65d45e38c3516d89
pangonghang <1966656576@qq.com>
2020-07-12 23:38:19 +0800

    地下城

--- task5.js
+++ task5.js
@@ -8263,22 +8263,22 @@
  * @param {number[][]} dungeon
  * @return {number}
  */
-var calculateMinimumHP = function(dungeon) {
-    let n = dungeon.length, 
-        m = dungeon[0].length,
-        dp = [];
+// var calculateMinimumHP = function(dungeon) {
+//     let n = dungeon.length, 
+//         m = dungeon[0].length,
+//         dp = [];
 
-    for (let i = 0; i <= n; ++i) {
-        dp.push(new Array(n+1).fill(Number.MAX_SAFE_INTEGER));
-    }
+//     for (let i = 0; i <= n; ++i) {
+//         dp.push(new Array(n+1).fill(Number.MAX_SAFE_INTEGER));
+//     }
 
-    dp[n][m - 1] = dp[n - 1][m] = 1;
+//     dp[n][m - 1] = dp[n - 1][m] = 1;
     
-    for (let i = n - 1; i >= 0; --i) {
-        for (let j = m - 1; j >= 0; --j) {
-            let minn = Math.min(dp[i + 1][j], dp[i][j + 1]);
-            dp[i][j] = Math.max(minn - dungeon[i][j], 1);
-        }
-    }
-    return dp[0][0];
-};
+//     for (let i = n - 1; i >= 0; --i) {
+//         for (let j = m - 1; j >= 0; --j) {
+//             let minn = Math.min(dp[i + 1][j], dp[i][j + 1]);
+//             dp[i][j] = Math.max(minn - dungeon[i][j], 1);
+//         }
+//     }
+//     return dp[0][0];
+// };

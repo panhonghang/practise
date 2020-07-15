04b53efb58ed3086588af7b597401747b3010d12
panhonhang <1966656576@qq.com>
2020-07-15 20:59:17 +0800

    二叉树

--- task5.js
+++ task5.js
@@ -8310,3 +8310,44 @@
 //     }
 //     return dp[0][0];
 // };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+    const dp = new Array(n+1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for(let i = 2; i < n; i++) {
+        for(let j = 1; j <= i; j++){
+            dp[i] += dp[j-1] * dp[i-j]
+        }
+    }
+
+    return dp[n];
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+    if(n<=1) return 1;
+    if(n==2) return 2;
+
+    let dp = new Array(n+1);
+        dp.fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 5;
+
+    for(let i = 4; i <= n; i++){
+        for(let j = 0; j < i; j++){
+            dp[i] += dp[j]*dp[i-j-1]
+        }
+    }
+    return dp[n]
+};

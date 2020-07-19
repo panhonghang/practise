9e400fadbcd35821cc8711e2c30c524fbc01792d
panhonhang <1966656576@qq.com>
2020-07-19 13:54:33 +0800

    戳气球

--- task5.js
+++ task5.js
@@ -8435,27 +8435,56 @@
  * @param {string} s3
  * @return {boolean}
  */
-var isInterleave = function(s1, s2, s3) {
-    let n1= s1.length;
-    let n2 = s2.length;
+// var isInterleave = function(s1, s2, s3) {
+//     let n1= s1.length;
+//     let n2 = s2.length;
     
-    if (n1+n2!=s3.length) return false
+//     if (n1+n2!=s3.length) return false
 
-    let dp = Array.from(new Array(n1+1), () => new Array(n2+1))
-    dp[0][0] = true
-    // 初始化
-    for(let i=1;i<=n1;i++) {
-        dp[i][0] = dp[i-1][0] && s1[i-1] == s3[i-1]
-    }
-    for(let i=1;i<=n2;i++) {
-        dp[0][i] = dp[0][i-1] && s2[i-1] == s3[i-1]
-    }
+//     let dp = Array.from(new Array(n1+1), () => new Array(n2+1))
+//     dp[0][0] = true
+//     // 初始化
+//     for(let i=1;i<=n1;i++) {
+//         dp[i][0] = dp[i-1][0] && s1[i-1] == s3[i-1]
+//     }
+//     for(let i=1;i<=n2;i++) {
+//         dp[0][i] = dp[0][i-1] && s2[i-1] == s3[i-1]
+//     }
 
-    for(let i =1;i<=n1;i++) {
-        for(let j=1;j<=n2;j++) {
-            dp[i][j] = dp[i-1][j] && s1[i-1] == s3[i-1+j] || dp[i][j-1] && s2[j-1] ==s3[i+j-1]
-        }
-    }
+//     for(let i =1;i<=n1;i++) {
+//         for(let j=1;j<=n2;j++) {
+//             dp[i][j] = dp[i-1][j] && s1[i-1] == s3[i-1+j] || dp[i][j-1] && s2[j-1] ==s3[i+j-1]
+//         }
+//     }
 
-    return dp[n1][n2]
-};
+//     return dp[n1][n2]
+// };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var maxCoins = function(nums) {
+//     if(nums.length == 0) return 0;
+
+//     nums = [1,...nums,1];
+
+//     let length = nums.length;
+
+//     let dp = Array.from(new Array(length), ()=> new Array(length).fill(0));
+
+//     for (let i = length-3; i > -1; i--) {
+//         for (let j =i+2;j < length;j++) {
+//             let max = 0;
+            
+//             for(let k = i+1;k<j;k++) {
+//                 let temp = dp[i][k] + dp[k][j] + nums[i] * nums[k] * nums[j]
+//                 if(temp>max) {
+//                     max = temp
+//                 }
+//             }
+//             dp[i][j] = max
+//         }
+//     }
+//     return dp[0][length-1]
+// };

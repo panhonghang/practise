04bd5fa9b69750f88869edc0ac086bc20fdc63ec
panhonghang <1966656576@qq.com>
2020-07-25 15:28:19 +0800

    数组分割

--- task5.js
+++ task5.js
@@ -8581,8 +8581,33 @@
  * @param {number} N
  * @return {boolean}
  */
-var divisorGame = function(N) {
-    if(N==1) return false;
-    if(N==2) return true;
-    return N%2 === 0;
+// var divisorGame = function(N) {
+//     if(N==1) return false;
+//     if(N==2) return true;
+//     return N%2 === 0;
+// };
+
+/**
+ * @param {number[]} nums
+ * @param {number} m
+ * @return {number}
+ */
+var splitArray = function(nums, m) {
+    let n = nums.length,
+        dp = Array.from(new Array(n + 1), ()=> new Array(m + 1).fill(Number.MAX_SAFE_INTEGER)),
+        sub = new Array(n +1).fill(0);
+    
+    for(let i = 0; i < n; i++) sub[i+1] = sub[i] + nums[i];
+
+    dp[0][0] = 0;
+
+    for(let i = 1; i <= n; i++) {
+        for(let j = 1; j <= m; j++) {
+            for(let k = 0; k < i; k++) {
+                dp[i][j] = Math.min(dp[i][j], Math.max(dp[k][j-1], sub[i] - sub[k]))
+            }
+        }
+    }
+
+    return dp[n][m]
 };

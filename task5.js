4f792e185f891c804c649a137b14985e288e8541
panhonghang <1966656576@qq.com>
2020-07-30 19:50:27 +0800

    拆分数字'

--- task5.js
+++ task5.js
@@ -9124,28 +9124,28 @@
  * @return {number}
  */
 
-const integerBreak = (n) => {
-    const memo = new Array(n + 1);
-    const dfs = (n) => {
-      if (memo[n]) return memo[n];
-      let res = 0;
-      for (let i = 1; i <= n - 1; i++) {
-        res = Math.max(res, i * (n - i), i * dfs(n - i));
-      }
-      return memo[n] = res;
-    };
-    return dfs(n);
-};
+// const integerBreak = (n) => {
+//     const memo = new Array(n + 1);
+//     const dfs = (n) => {
+//       if (memo[n]) return memo[n];
+//       let res = 0;
+//       for (let i = 1; i <= n - 1; i++) {
+//         res = Math.max(res, i * (n - i), i * dfs(n - i));
+//       }
+//       return memo[n] = res;
+//     };
+//     return dfs(n);
+// };
 
-const integerBreak = (n) => {
-    const dp = new Array(n + 1);
-    dp[1] = 1;  
-    dp[2] = 1; 
-    for (let i = 3; i <= n; i++) {
-      dp[i] = 0;
-      for (let j = 1; j <= i - j; j++) {
-        dp[i] = Math.max(dp[i], j * (i - j), j * dp[i - j]);
-      }
-    }
-    return dp[n];
-};
+// const integerBreak = (n) => {
+//     const dp = new Array(n + 1);
+//     dp[1] = 1;  
+//     dp[2] = 1; 
+//     for (let i = 3; i <= n; i++) {
+//       dp[i] = 0;
+//       for (let j = 1; j <= i - j; j++) {
+//         dp[i] = Math.max(dp[i], j * (i - j), j * dp[i - j]);
+//       }
+//     }
+//     return dp[n];
+// };

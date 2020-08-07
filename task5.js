91ebdee1d1b0ca996999a16127beab98b3b5c151
panhonghang <1966656576@qq.com>
2020-08-07 14:04:53 +0800

    判断相同的树

--- task5.js
+++ task5.js
@@ -9358,3 +9358,30 @@
 //     }
 //     return true
 // }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+// var isSameTree = function(p, q) {
+//     const fn = (node1, node2) => {
+//         if(!node1 || !node2) return node1 === node2;
+
+//         if(node1.val === node2.val){
+//             return fn(node1.left, node2.left) && fn(node1.right, node2.right);
+//         } else {
+//             return false;
+//         }
+//     }
+
+//     return fn(p, q);
+// };

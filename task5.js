17a7b76ad682dc1b540770c03008373919072409
panhonghang <1966656576@qq.com>
2020-08-20 22:37:14 +0800

    扫雷

--- task5.js
+++ task5.js
@@ -13463,3 +13463,51 @@ const div2 = (x) => x / 2
 //   }
 //   return res;
 // };
+/**
+ * @param {character[][]} board
+ * @param {number[]} click
+ * @return {character[][]}
+ */
+// const updateBoard = (board, click) => {
+//   const m = board.length;
+//   const n = board[0].length;
+//   const dx = [1, 1, 1, -1, -1, -1, 0, 0];
+//   const dy = [1, 0, -1, 0, 1, -1, 1, -1];
+//   const inBound = (x, y) => x >= 0 && x < m && y >= 0 && y < n;
+
+//   const bfs = (x, y) => {
+//     const queue = [[x, y]];
+//     while (queue.length) {
+//       const [x, y] = queue.shift();
+//       let count = 0;
+//       for (let i = 0; i < 8; i++) {
+//         const nX = x + dx[i];
+//         const nY = y + dy[i];
+//         if (inBound(nX, nY) && board[nX][nY] == 'M') {
+//           count++;
+//         }
+//       }
+//       if (count == 0) {
+//         board[x][y] = 'B';
+//         for (let i = 0; i < 8; i++) {
+//           const nX = x + dx[i];
+//           const nY = y + dy[i];
+//           if (inBound(nX, nY) && board[nX][nY] == 'E') {
+//             board[nX][nY] = 'B'; // 变成一个非E字符就行，标记该节点被访问过了
+//             queue.push([nX, nY]);
+//           }
+//         }
+//       } else {
+//         board[x][y] = count + '';
+//       }
+//     }
+//   };
+
+//   const [cX, cY] = click;
+//   if (board[cX][cY] == 'M') {
+//     board[cX][cY] = 'X';
+//   } else {
+//     bfs(cX, cY);
+//   }
+//   return board;
+// };

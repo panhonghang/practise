20a1bfd452b1e55da261c32d4c3f10e48b4f00e1
panhonghang <1966656576@qq.com>
2020-08-25 13:54:41 +0800

    dfs

--- task5.js
+++ task5.js
@@ -13596,3 +13596,30 @@ const div2 = (x) => x / 2
 //   let sDouble1 = sDouble.slice(1,-1)
 //   return sDouble1.indexOf(s)!=-1
 // };
+
+// const findSubsequences = (nums) => {
+//   const res = [];
+//   const len = nums.length;
+//   const set = new Set();
+  
+//   const dfs = (start, path) => {
+//     if (path.length >= 2) {
+//       const str = path.join(','); 
+//       if (!set.has(str)) {        
+//         res.push(path.slice());
+//         set.add(str);
+//       }
+//     }
+//     for (let i = start; i < len; i++) {
+//       const prev = path[path.length - 1];
+//       const cur = nums[i];
+//       if (path.length == 0 || prev <= cur) {
+//         path.push(cur);    
+//         dfs(i + 1, path);  
+//         path.pop();        
+//       }
+//     }
+//   };
+//   dfs(0, []);
+//   return res;
+// };

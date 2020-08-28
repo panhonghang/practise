b50ed540ecb67f70bcf8194a8bbe958824ddc465
pangonghang <1966656576@qq.com>
2020-08-28 23:31:39 +0800

    回到起点

--- task5.js
+++ task5.js
@@ -13658,3 +13658,29 @@ const div2 = (x) => x / 2
 
 //   return resArr;
 // };
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+// var judgeCircle = function(moves) {
+//   let m = moves.length;
+//   let x = 0,y=0;
+//   if(m%2) return false; // 奇数不可能回到原点
+//   for(let i=0;i<m;i++){
+//       switch(moves[i]){ // 坐标加减
+//           case 'U':
+//               y++
+//               break;
+//           case 'R':
+//               x++
+//               break;
+//           case 'D':
+//               y--
+//               break;
+//           case 'L':
+//               x--
+//               break;
+//       }
+//   }
+//   return x===0&&y===0; // 加减后为原点的话 返回true
+// };

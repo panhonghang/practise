afc7121c8fb14378806717217675ce2ab4f2f7db
pangonghang <1966656576@qq.com>
2020-09-01 18:56:42 +0800

    判断输赢

--- task5.js
+++ task5.js
@@ -13712,3 +13712,17 @@ const div2 = (x) => x / 2
 // var reverseWords = function(s) {
 //   return s.split(' ').map(key=>key.split('').reverse().join('')).join(' ');
 // };
+
+// const PredictTheWinner = (nums) => {
+
+//   const helper = (i, j) => { // i，j是两端的索引
+//     if (i == j) {    // 递归的出口，此时只有一个选择，并且没有剩余的可选
+//       return nums[i];
+//     }
+//     const pickI = nums[i] - helper(i + 1, j); // 选择左端
+//     const pickJ = nums[j] - helper(i, j - 1); // 选择右端
+//     return Math.max(pickI, pickJ);            // 取较大者
+//   };
+
+//   return helper(0, nums.length - 1) >= 0;
+// };

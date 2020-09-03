657810ac81ffa39594885e314526577a1f6dae87
pangonghang <1966656576@qq.com>
2020-09-03 22:59:58 +0800

    N皇后

--- task5.js
+++ task5.js
@@ -13817,3 +13817,22 @@ const div2 = (x) => x / 2
 //     || curState === STATE.EXP_INT
 //     || curState === STATE.END_SPACE;
 // };
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+// var solveNQueens = function(n) {
+//   let result = [];
+  
+//   function dfs(cols) {
+//       let row = cols.length;
+//       if (row === n) return result.push(cols.map(col => '.'.repeat(col) + 'Q' + '.'.repeat(n - col -1)));
+//       for (let col = 0; col < n; col++) {
+//           if(cols.some((exCol, exRow) => (exCol === col || exRow - exCol === row - col || exRow + exCol === row + col))) continue;
+//           dfs(cols.concat(col))
+//       } 
+//   }
+//   dfs([]);
+
+//   return result;
+// };

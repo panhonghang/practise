7b46f45df59c76caae957afe927c84f14ce296ad
pangonghang <1966656576@qq.com>
2020-09-07 19:56:30 +0800

    347. 前 K 个高频元素

--- task5.js
+++ task5.js
@@ -13966,3 +13966,27 @@ const div2 = (x) => x / 2
 
 //   return res;
 // };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+// var topKFrequent = function(nums, k) {
+//   const map = new Map();
+  
+//     nums.forEach(item=>{
+//       if(map.has(item)) {
+//         map.set(item, map.get(item) + 1);
+//       } else {
+//         map.set(item, 1);
+//       }
+//     })
+
+//   return Array.from(map)
+//   .sort((a, b) => b[1] - a[1])
+//   .splice(0, k)
+//   .map(item => item[0])
+// };
+
+// console.log(topKFrequent([1,1,1,2,2,3], 2))

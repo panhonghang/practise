a72ff0b73f71bdb5925cc371de9e4d3c5b956beb
pangonghang <1966656576@qq.com>
2020-09-07 20:55:16 +0800

    862. 和至少为 K 的最短子数组

--- task5.js
+++ task5.js
@@ -14020,3 +14020,41 @@ const div2 = (x) => x / 2
 
 //   return len === Number.MAX_SAFE_INTEGER ? -1 : len;
 // };
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+// var shortestSubarray = function(A, K) {
+//   let len = Number.MAX_SAFE_INTEGER,
+//       sum = 0,
+//       pre = 0,
+//       next = 0;
+  
+//   if(A[0] === K) return 1; 
+
+//   while(pre < A.length - 1) {
+//     if(A[pre] === K) return 1;
+//     next = pre;
+//     sum = 0;
+
+//     while(sum < K && next < A.length) {
+//       sum += A[next];
+//       if(sum <= 0) {
+//         pre = next;
+//         sum = 0;
+//       }
+//       if(sum < K) next++;
+//     }
+    
+//     if( sum >= K ) {
+//         if(A[pre] > 0) len = Math.min(len, next - pre + 1);
+//         pre++
+//     } else {
+//         break;
+//     }
+//   }
+
+//   return len === Number.MAX_SAFE_INTEGER ? -1 : len;
+// };

f3780d5ae74892fbcf36995c86e5183461a0fbea
pangonghang <1966656576@qq.com>
2020-09-27 14:29:25 +0800

    公共父元素

--- task5.js
+++ task5.js
@@ -14628,3 +14628,22 @@ const div2 = (x) => x / 2
 
 //     return resArr;
 // };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+// var lowestCommonAncestor = function(root, p, q) {
+//     if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
+//     if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+//     return root;
+// };

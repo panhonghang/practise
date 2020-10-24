92af4a8d7f0b3e29b9ce531178ff4bbc8dd87fbd
pangonghang <1966656576@qq.com>
2020-10-24 22:30:57 +0800

    视频拼接 Java 版本

--- LeetCode-Java/leetcode.java
+++ LeetCode-Java/leetcode.java
@@ -119,24 +119,69 @@ import java.util.HashMap;
 //     }
 // }
 
-class Solution {
-    public int minFallingPathSum(int[][] arr) {
-        int[] dp = arr[0];
-        int[] temp = dp.clone();
-        int min = 0;
+// class Solution {
+//     public int minFallingPathSum(int[][] arr) {
+//         int[] dp = arr[0];
+//         int[] temp = dp.clone();
+//         int min = 0;
         
-        for(int i = 1; i < arr.length; i++) {
-            temp = dp.clone();
-            Arrays.sort(temp);
-            for(int j = 0; j < dp.length; j++) {
-                min = temp[0];
-                if(dp[j] == temp[0]) min = temp[1];
-                dp[j] = min + arr[i][j];
-            }        
+//         for(int i = 1; i < arr.length; i++) {
+//             temp = dp.clone();
+//             Arrays.sort(temp);
+//             for(int j = 0; j < dp.length; j++) {
+//                 min = temp[0];
+//                 if(dp[j] == temp[0]) min = temp[1];
+//                 dp[j] = min + arr[i][j];
+//             }        
+//         }
+
+//         Arrays.sort(dp);
+
+//         return dp[0];
+//     }
+// }
+
+class Solution {
+    public int videoStitching(int[][] clips, int T) {
+        if (T == 0) {
+            return 0;
         }
 
-        Arrays.sort(dp);
+        int[] arr = new int[T]; // 已知时长T <= 100，所以可以用数组排序
+        for (int[] clip : clips) {
+            if (clip[0] < clip[1] && clip[0] < T && arr[clip[0]] < clip[1]) {
+                arr[clip[0]] = clip[1]; 
+            }
+        }
 
-        return dp[0];
+        int count = 0; // 个数
+        int preEnd = -1; // 之前所有片段组end的最大值
+        int maxEnd = 0; // 当前所有片段组end的最大值
+
+        for (int start = 0; start < arr.length; start++) {
+            if (start > maxEnd) { // 片段不连续，非法
+                return -1;
+            }
+
+            int end = arr[start];
+            if (end == 0) {
+                continue;
+            }
+
+            if (end > maxEnd) { // 当前片段end超过之前所有的片段end
+                if (start > preEnd) { // true：代表当前片段为新增片段。false：代表当前片段可以替换前一个片段
+                    preEnd = maxEnd;
+                    count++;
+                }
+
+                if (end >= T) { // 当前片段end大于时长T时，返回个数
+                    return count;
+                }
+
+                maxEnd = end;
+            }
+        }
+
+        return -1;
     }
 }

c559ad4d009e982a9de8442f8dba50301c95eeaf
pangonghang <1966656576@qq.com>
2020-10-25 14:35:03 +0800

    数组中的最长山脉 改进 JavaScript 版本

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -968,6 +968,10 @@ function longestMountain(A: number[]): number {
     let height:number = 0;
 
     for(let i = 1; i < A.length - 1; i++) {
+        while(A[i] < A[i+1]) {
+            i++;
+            if(i + 1 == A.length) break;
+        }
         let pre:number = i,
             next:number = i;
         //i to pre

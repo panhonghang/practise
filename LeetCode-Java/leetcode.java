936989c078c3cc281d0bb4d4d377a3b689b3a507
pangonghang <1966656576@qq.com>
2020-10-25 14:22:46 +0800

    数组中的最长山脉 Java 版本

--- LeetCode-Java/leetcode.java
+++ LeetCode-Java/leetcode.java
@@ -141,47 +141,68 @@ import java.util.HashMap;
 //     }
 // }
 
+// class Solution {
+//     public int videoStitching(int[][] clips, int T) {
+//         if (T == 0) {
+//             return 0;
+//         }
+
+//         int[] arr = new int[T]; // 已知时长T <= 100，所以可以用数组排序
+//         for (int[] clip : clips) {
+//             if (clip[0] < clip[1] && clip[0] < T && arr[clip[0]] < clip[1]) {
+//                 arr[clip[0]] = clip[1]; 
+//             }
+//         }
+
+//         int count = 0; // 个数
+//         int preEnd = -1; // 之前所有片段组end的最大值
+//         int maxEnd = 0; // 当前所有片段组end的最大值
+
+//         for (int start = 0; start < arr.length; start++) {
+//             if (start > maxEnd) { // 片段不连续，非法
+//                 return -1;
+//             }
+
+//             int end = arr[start];
+//             if (end == 0) {
+//                 continue;
+//             }
+
+//             if (end > maxEnd) { // 当前片段end超过之前所有的片段end
+//                 if (start > preEnd) { // true：代表当前片段为新增片段。false：代表当前片段可以替换前一个片段
+//                     preEnd = maxEnd;
+//                     count++;
+//                 }
+
+//                 if (end >= T) { // 当前片段end大于时长T时，返回个数
+//                     return count;
+//                 }
+
+//                 maxEnd = end;
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
+
 class Solution {
-    public int videoStitching(int[][] clips, int T) {
-        if (T == 0) {
-            return 0;
+    public int longestMountain(int[] A) {
+        int height = 0;
+
+        for(int i = 1; i < A.length - 1; i++) {
+            int pre = i,
+                next = i;
+            //i to pre
+            while(pre > 0 && A[pre] > A[pre - 1]) pre--;
+            //i to next
+            while(next < A.length - 1 && A[next] > A[next+1]) next++;
+
+            if(pre != i && next != i) height = Math.max(height, next - pre + 1);
+
+            i = next;
         }
 
-        int[] arr = new int[T]; // 已知时长T <= 100，所以可以用数组排序
-        for (int[] clip : clips) {
-            if (clip[0] < clip[1] && clip[0] < T && arr[clip[0]] < clip[1]) {
-                arr[clip[0]] = clip[1]; 
-            }
-        }
-
-        int count = 0; // 个数
-        int preEnd = -1; // 之前所有片段组end的最大值
-        int maxEnd = 0; // 当前所有片段组end的最大值
-
-        for (int start = 0; start < arr.length; start++) {
-            if (start > maxEnd) { // 片段不连续，非法
-                return -1;
-            }
-
-            int end = arr[start];
-            if (end == 0) {
-                continue;
-            }
-
-            if (end > maxEnd) { // 当前片段end超过之前所有的片段end
-                if (start > preEnd) { // true：代表当前片段为新增片段。false：代表当前片段可以替换前一个片段
-                    preEnd = maxEnd;
-                    count++;
-                }
-
-                if (end >= T) { // 当前片段end大于时长T时，返回个数
-                    return count;
-                }
-
-                maxEnd = end;
-            }
-        }
-
-        return -1;
+        return height < 3 ? 0 : height;
     }
 }

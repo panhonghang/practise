5ec9032cdb48866c86c7fffac3701e8d16aade87
pangonghang <1966656576@qq.com>
2020-11-18 19:48:22 +0800

    加油站 Java 版本

--- LeetCode-Java/TreeNode.java
+++ LeetCode-Java/TreeNode.java
@@ -318,15 +318,38 @@ import java.util.*;
 //     }
 // }
 
-class Solution {
-    public int[][] reconstructQueue(int[][] people) {
-        Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
+// class Solution {
+//     public int[][] reconstructQueue(int[][] people) {
+//         Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
 
-        LinkedList<int[]> list = new LinkedList<>();
-        for (int[] i : people) {
-            list.add(i[1], i);
+//         LinkedList<int[]> list = new LinkedList<>();
+//         for (int[] i : people) {
+//             list.add(i[1], i);
+//         }
+
+//         return list.toArray(new int[list.size()][2]);
+//     }
+// }
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sum = 0,
+            start = 0;
+
+        if(sum(gas) < sum(cost)) return -1;
+
+        for(int i = 0; i < gas.length; i++){
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                start = i + 1;
+                sum = 0;
+            }
         }
 
-        return list.toArray(new int[list.size()][2]);
+        return start;
+    }
+
+    public int sum(int[] arr) {
+        return Arrays.stream(arr).reduce(0, (acc, n) -> acc + n);
     }
 }

bf1c50b679cdc9ecd3f8e0baab9e9965cf6516c6
pangonghang <1966656576@qq.com>
2020-11-28 19:20:02 +0800

    翻转对 java版本

--- LeetCode-Java/TreeNode.java
+++ LeetCode-Java/TreeNode.java
@@ -496,23 +496,81 @@ import java.util.stream.Stream;
 //     }
 // }
 
-class Solution {
-    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int a : A) {
-            for (int b : B) {
-                map.put(a + b, map.getOrDefault(a + b, 0) + 1);
-            }
-        }
+// class Solution {
+//     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int a : A) {
+//             for (int b : B) {
+//                 map.put(a + b, map.getOrDefault(a + b, 0) + 1);
+//             }
+//         }
 
-        int res = 0;
-        for (int c : C) {
-            for (int d : D) {
-                if (map.containsKey(0 - c - d)) {
-                    res += map.get(0 - c - d);
-                }
+//         int res = 0;
+//         for (int c : C) {
+//             for (int d : D) {
+//                 if (map.containsKey(0 - c - d)) {
+//                     res += map.get(0 - c - d);
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+class Solution {
+    // 一个递增排列的数组，用于二分查找
+    private ArrayList<Long> array;
+
+    // 将value添加进有序数组array中
+    private void add(long value) {
+        int low = 0, high = array.size(), mid;
+        long v;
+        while (low < high) {
+            mid = (low + high)>>1;
+            v = array.get(mid);
+            if(v > value) {
+                high = mid;
+            } else if(v == value) {
+                low = mid;
+                break;
+            } else {
+                low = mid + 1;
             }
         }
-        return res;
+        array.add(low, value);
+    }
+
+    // 从有序数组array中找到比value小的元素个数
+    private int getLt(int value) {
+        int low =0, high = array.size(), mid;
+        long v;
+        while (low < high) {
+            mid = (low + high)>>1;
+            v = array.get(mid);
+            if(v < value) {
+                low = mid + 1;
+            } else if(v >= value){
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    public int reversePairs(int[] nums) {
+        int n = nums.length;
+        if(n <= 1)return 0;
+
+        int ans = 0;
+        array = new ArrayList<>();
+        // 一开始，将最后一个元素的2倍加入array
+        array.add(((long)nums[n-1])<<1);
+
+        for (int i = n-2; i >= 0; i--) {
+            // 找到当前array中比nums[i]小的元素个数
+            ans += getLt(nums[i]);
+            // 将nums[i]的2倍添加进array
+            add(((long)nums[i])<<1);
+        }
+        return ans;
     }
 }

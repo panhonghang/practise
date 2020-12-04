927f2ad8fe1cc0902424ab4a4495727c4cc5212d
pangonghang <1966656576@qq.com>
2020-12-04 20:15:06 +0800

    分割数组为连续子序列

--- LeetCode-Java/TreeNode.java
+++ LeetCode-Java/TreeNode.java
@@ -679,3 +679,31 @@ import java.util.stream.Stream;
 //         return compare(nums1, p1 + 1, nums2, p2 + 1);
 //     }
 // }
+
+class Solution {
+    public boolean isPossible(int[] nums) {
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<Integer, PriorityQueue<Integer>>();
+        for (int x : nums) {
+            if (!map.containsKey(x)) {
+                map.put(x, new PriorityQueue<Integer>());
+            }
+            if (map.containsKey(x - 1)) {
+                int prevLength = map.get(x - 1).poll();
+                if (map.get(x - 1).isEmpty()) {
+                    map.remove(x - 1);
+                }
+                map.get(x).offer(prevLength + 1);
+            } else {
+                map.get(x).offer(1);
+            }
+        }
+        Set<Map.Entry<Integer, PriorityQueue<Integer>>> entrySet = map.entrySet();
+        for (Map.Entry<Integer, PriorityQueue<Integer>> entry : entrySet) {
+            PriorityQueue<Integer> queue = entry.getValue();
+            if (queue.peek() < 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

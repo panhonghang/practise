86fe653688f984472f55b077b48bd8e9861290d0
pangonghang <1966656576@qq.com>
2020-12-07 16:57:46 +0800

    翻转矩阵的得分

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -1895,6 +1895,27 @@ function generate(numRows: number): number[][] {
     return res;
 };
 
+function matrixScore(A: number[][]): number {
+    const m:number = A.length, 
+          n:number = A[0].length;
+
+    let ret:number = m * (1 << (n - 1));
+
+    for (let j = 1; j < n; j++) {
+        let nOnes = 0;
+        for (let i = 0; i < m; i++) {
+            if (A[i][0] === 1) {
+                nOnes += A[i][j];
+            } else {
+                nOnes += (1 - A[i][j]); // 如果这一行进行了行反转，则该元素的实际取值为 1 - A[i][j]
+            }
+        }
+        const k = Math.max(nOnes, m - nOnes);
+        ret += k * (1 << (n - j - 1));
+    }
+    return ret;
+};
+
 export {
     sortString,
     countNodes,

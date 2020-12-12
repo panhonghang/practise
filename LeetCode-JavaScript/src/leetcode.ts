8bc54fa98a3954a564f1ce23216bfcf8c7e1af7a
pangonghang <1966656576@qq.com>
2020-12-12 22:34:43 +0800

    摆动序列

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -2033,7 +2033,26 @@ function predictPartyVictory(senate: string): string {
         }
     return radiant.length > 0 ? "Radiant" : "Dire";
 };
-
+function wiggleMaxLength(nums: number[]): number {
+    const n:number = nums.length;
+    if (n < 2) return n;
+    const up:number[] = new Array(n).fill(0);
+    const down:number[] = new Array(n).fill(0);
+    up[0] = down[0] = 1;
+    for (let i = 1; i < n; i++) {
+        if (nums[i] > nums[i - 1]) {
+            up[i] = Math.max(up[i - 1], down[i - 1] + 1);
+            down[i] = down[i - 1];
+        } else if (nums[i] < nums[i - 1]) {
+            up[i] = up[i - 1];
+            down[i] = Math.max(up[i - 1] + 1, down[i - 1]);
+        } else {
+            up[i] = up[i - 1];
+            down[i] = down[i - 1];
+        }
+    }
+    return Math.max(up[n - 1], down[n - 1]);
+};
 export {
     lemonadeChange,
     uniquePaths,

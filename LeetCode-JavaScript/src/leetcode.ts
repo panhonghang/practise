7d1115c09d7ac50472ce529dfe3360c8d7ec6cf5
pangonghang <1966656576@qq.com>
2020-12-19 17:30:12 +0800

    旋转照片

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -2177,7 +2177,25 @@ function findTheDifference(s: string, t: string): string {
     return String.fromCharCode(code);
 };
 
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+    const n:number = matrix.length;
+    
+    for (let i = 0; i < Math.floor(n / 2); ++i) {
+        for (let j = 0; j < Math.floor((n + 1) / 2); ++j) {
+            const temp = matrix[i][j];
+            matrix[i][j] = matrix[n - j - 1][i];
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+            matrix[j][n - i - 1] = temp;
+        }
+    }
+};
+
 export {
+    rotate,
     findTheDifference,
     maxProfit2,
     maxProfit1,

3a9440e1c808f47666a1b7dac7a3561d96428053
pangonghang <1966656576@qq.com>
2020-12-21 13:19:12 +0800

    使用最小花费爬楼梯

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -2211,6 +2211,16 @@ function removeDuplicateLetters(s: string): string {
     return stack.join('')
 };
 
+function minCostClimbingStairs(cost: number[]): number {
+    const dp:number[] = new Array(cost.length + 1).fill(0);
+
+    for (let i = 2; i <= cost.length; i++) {
+        dp[i] = Math.min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+    }
+
+    return dp[cost.length];
+};
+
 export {
     rotate,
     findTheDifference,

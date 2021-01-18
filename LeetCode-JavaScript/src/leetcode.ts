0d31e7ec56ddaa766f5b22dd0afe406c785a9538
pangonghang <1966656576@qq.com>
2021-01-18 21:34:24 +0800

    账户合并

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -2971,6 +2971,54 @@ function checkStraightLine(coordinates: number[][]): boolean {
 
     return true;
 };
+
+function accountsMerge(accounts: string[][]): string[][] {
+    const emailToIndex = new Map();
+    const emailToName = new Map();
+    let emailsCount = 0;
+    for (const account of accounts) {
+        const name = account[0];
+        const size = account.length;
+        for (let i = 1; i < size; i++) {
+            const email = account[i];
+            if (!emailToIndex.has(email)) {
+                emailToIndex.set(email, emailsCount++);
+                emailToName.set(email, name);
+            }
+        }
+    }
+
+    const uf = new UnionFind(emailsCount);
+    for (const account of accounts) {
+        const firstEmail = account[1];
+        const firstIndex = emailToIndex.get(firstEmail);
+        const size = account.length;
+        for (let i = 2; i < size; i++) {
+            const nextEmail = account[i];
+            const nextIndex = emailToIndex.get(nextEmail);
+            uf.merge(firstIndex, nextIndex);
+        }
+    }
+
+    const indexToEmails = new Map();
+    for (const email of emailToIndex.keys()) {
+        const index = uf.find(emailToIndex.get(email));
+        const account = indexToEmails.get(index) ? indexToEmails.get(index) : [];
+        account.push(email);
+        indexToEmails.set(index, account);
+    }
+    const merged = [];
+    for (const emails of indexToEmails.values()) {
+        emails.sort();
+        const name = emailToName.get(emails[0]);
+        const account = [];
+        account.push(name);
+        account.push(...emails);
+        merged.push(account);
+    }
+    return merged;
+};
+
 export {
     hitBricks,
     sortItems,

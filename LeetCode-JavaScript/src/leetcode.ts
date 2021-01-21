998a80c64a1df142052c854a8ba041fa981205ad
pangonghang <1966656576@qq.com>
2021-01-21 18:47:02 +0800

    找到最小生成树里的关键边和伪关键边

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -3063,6 +3063,94 @@ function maximumProduct(nums: number[]): number {
     return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
 };
 
+function findCriticalAndPseudoCriticalEdges(n: number, edges: number[][]): number[][] {
+    const m = edges.length;
+    for (const [i, edge] of edges.entries()) {
+        edge.push(i);
+    }
+    edges.sort((a, b) => a[2] - b[2]);
+
+    // 计算 value
+    const uf_std = new UnionFind3(n);
+    let value = 0;
+    for (let i = 0; i < m; i++) {
+        if (uf_std.unite(edges[i][0], edges[i][1])) {
+            value += edges[i][2];
+        }
+    }
+
+    const ans:number[][] = [[], []];
+
+    for (let i = 0; i < m; i++) {
+        // 判断是否是关键边
+        let uf = new UnionFind3(n);
+        let v = 0;
+        for (let j = 0; j < m; j++) {
+            if (i !== j && uf.unite(edges[j][0], edges[j][1])) {
+                v += edges[j][2];
+            }
+        }
+        if (uf.setCount !== 1 || (uf.setCount === 1 && v > value)) {
+            ans[0].push(edges[i][3]);
+            continue;
+        }
+
+        // 判断是否是伪关键边
+        uf = new UnionFind3(n);
+        uf.unite(edges[i][0], edges[i][1]);
+        v = edges[i][2];
+        for (let j = 0; j < m; j++) {
+            if (i !== j && uf.unite(edges[j][0], edges[j][1])) {
+                v += edges[j][2];
+            }
+        }
+        if (v === value) {
+            ans[1].push(edges[i][3]);
+        }
+    }
+    return ans;
+};
+
+class UnionFind3 {
+    private parent:number[];
+    private size;
+    public setCount;
+
+    constructor (n:number) {
+        this.parent = new Array(n).fill(0).map((element, index) => index);
+        this.size = new Array(n).fill(1);
+        // 当前连通分量数目
+        this.setCount = n;
+    }
+
+    findset (x:number) {
+        if (this.parent[x] === x) {
+            return x;
+        }
+        this.parent[x] = this.findset(this.parent[x]);
+        return this.parent[x];
+    }
+
+    unite (a:number, b:number) {
+        let x = this.findset(a), y = this.findset(b);
+        if (x === y) {
+            return false;
+        }
+        if (this.size[x] < this.size[y]) {
+            [x, y] = [y, x];
+        }
+        this.parent[y] = x;
+        this.size[x] += this.size[y];
+        this.setCount -= 1;
+        return true;
+    }
+
+    connected (a:number, b:number) {
+        const x = this.findset(a), y = this.findset(b);
+        return x === y;
+    }
+}
+
 export {
     hitBricks,
     sortItems,

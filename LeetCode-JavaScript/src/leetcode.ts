5b30ed8533a2a9aff4a2cd186a21bf7d63866658
pangonghang <1966656576@qq.com>
2021-01-31 21:25:45 +0800

    相似字符串组

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -3462,6 +3462,48 @@ const check = (grid:number[][], threshold:number) => {
     return visited[n - 1][n - 1];
 };
 
+function numSimilarGroups(strs: string[]): number {
+    const n:number = strs.length;
+    const m:number = strs[0].length;
+    const f:number[] = new Array(n).fill(0).map((element, index) => index);
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            const fi = find(i), fj = find(j);
+            if (fi === fj) {
+                continue;
+            }
+            if (check(strs[i], strs[j], m)) {
+                f[fi] = fj;
+            }
+        }
+    }
+    let ret = 0;
+    for (let i = 0; i < n; i++) {
+        if (f[i] === i) {
+            ret++;
+        }
+    }
+    return ret;
+
+    function find(x:number):number {
+        return f[x] === x ? x : (f[x] = find(f[x]));
+    }
+
+    function check(a:string, b:string, len:number):boolean {
+        let num = 0;
+        for (let i = 0; i < len; i++) {
+            if (a[i] !== b[i]) {
+                num++;
+                if (num > 2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
 export {
     regionsBySlashes,
     hitBricks,

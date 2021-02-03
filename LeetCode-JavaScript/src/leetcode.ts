562d4a5aa7b82bd4bff2284d036a12a27edd58aa
pangonghang <1966656576@qq.com>
2021-02-03 19:39:21 +0800

    滑动窗口中位数

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -3540,6 +3540,68 @@ function characterReplacement(s: string, k: number): number {
     return right - left;
 };
 
+function medianSlidingWindow(nums: number[], k: number): number[] {
+    let l:number = 0, 
+        r:number = 0;
+        
+    const arr:number[] = [],
+          isOdd:boolean = k % 2 === 1,
+          ans:number[] = [];
+
+    while (r < nums.length) {
+        insert(arr, nums[r++]);
+        if (arr.length === k) {
+            const mid = k >> 1;
+            if (isOdd) {
+                ans.push(arr[mid]);
+            } else {
+                ans.push((arr[mid] + arr[mid - 1]) / 2);
+            }
+            remove(arr, nums[l++]);
+        }
+    }
+    return ans;
+
+    function insert(arr:number[], num:number) : void {
+        const len:number = arr.length;
+        if (len === 0) {
+            arr.push(num);
+        } else {
+            if (arr[0] > num) {
+                arr.unshift(num);
+            } else if (arr[len - 1] <= num) {
+                arr.push(num);
+            } else {
+                const index = search(arr, num);
+                arr.splice(index, 0, num);
+            }
+        }
+    }
+
+    function search(arr:number[], num:number) : number {
+        let l:number = 0,
+            r:number = arr.length - 1;
+        
+        while (l <= r) {
+            const mid:number = (l + r) >> 1;
+            if (arr[mid] > num) {
+                r = mid - 1
+            } else if (arr[mid] < num) {
+                l = mid + 1
+            } else {
+                return mid
+            }
+        }
+
+        return l
+    }
+
+    function remove(arr:number[], num:number) : void {
+        const index:number = arr.indexOf(num);
+        arr.splice(index, 1);
+    }
+};
+
 export {
     characterReplacement,
     regionsBySlashes,

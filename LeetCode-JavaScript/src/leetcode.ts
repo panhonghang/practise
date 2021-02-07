d7711bb8f078a2665b478a8fa5db90d047390154
pangonghang <1966656576@qq.com>
2021-02-07 20:14:59 +0800

    非递减数列

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -3678,6 +3678,26 @@ function maxScore(cardPoints: number[], k: number): number {
     return maxValue;
 };
 
+function checkPossibility(nums: number[]): boolean {
+    let count: number = 0;
+    for (let i = 1, len = nums.length; i < len; i++) {
+        if (nums[i - 1] > nums[i])  {
+            if (nums[i - 2] > nums[i]) {
+                nums[i] = nums[i - 1];
+            } else {
+                nums[i - 1] = nums[i];
+            }
+            count++;
+        }
+
+        if (count > 1) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
 export {
     characterReplacement,
     regionsBySlashes,

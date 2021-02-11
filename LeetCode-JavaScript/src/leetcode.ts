a2e188fbe2a5623a6cc9fba52f79b1b5efc0cdc8
pangonghang <1966656576@qq.com>
2021-02-11 13:26:50 +0800

    数据流中的第 K 大元素

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -3841,6 +3841,118 @@ function checkInclusion(s1: string, s2: string): boolean {
     return false;
 };
 
+class KthLargest {
+    private k:number;
+    private heap:MinHeap;
+    constructor(k: number, nums: number[]) {
+        this.k = k;
+        this.heap = new MinHeap();
+        for (const x of nums) {
+            this.add(x);
+        }
+    }
+
+    public add(val: number): number | null {
+        this.heap.offer(val);
+        if (this.heap.size() > this.k) {
+            this.heap.poll();
+        }
+        return this.heap.peek();
+    }
+}
+
+interface comparator {
+    (a: number, b: number):number
+};
+
+class MinHeap {
+    private data:number[];
+    private comparator: comparator;
+    
+    constructor(data:number[] = []) {
+        this.data = data;
+        this.comparator = (a, b) => a - b;
+        this.heapify();
+    }
+
+    private heapify():void {
+        if (this.size() < 2) return;
+        for (let i = 1; i < this.size(); i++) {
+            this.bubbleUp(i);
+        }
+    }
+
+    public peek():number | null {
+        if (this.size() === 0) return null;
+        return this.data[0];
+    }
+
+    public offer(value:number):void {
+        this.data.push(value);
+        this.bubbleUp(this.size() - 1);
+    }
+
+    public poll():null | number {
+        if (this.size() === 0) {
+            return null;
+        }
+        const result:number = this.data[0];
+        const last:number = this.data.pop()!;
+        if (this.size() !== 0) {
+            this.data[0] = last;
+            this.bubbleDown(0);
+        }
+        return result;
+    }
+
+    private bubbleUp(index:number):void {
+        while (index > 0) {
+            const parentIndex = (index - 1) >> 1;
+            if (this.comparator(this.data[index], this.data[parentIndex]) < 0) {
+                this.swap(index, parentIndex);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private bubbleDown(index:number):void {
+        const lastIndex:number = this.size() - 1;
+        while (true) {
+            const leftIndex:number = index * 2 + 1;
+            const rightIndex:number = index * 2 + 2;
+            let findIndex = index;
+            if (leftIndex <= lastIndex && this.comparator(this.data[leftIndex], this.data[findIndex]) < 0) {
+                findIndex = leftIndex;
+            }
+            if (rightIndex <= lastIndex && this.comparator(this.data[rightIndex], this.data[findIndex]) < 0) {
+                findIndex = rightIndex;
+            }
+            if (index !== findIndex) {
+                this.swap(index, findIndex);
+                index = findIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private swap(index1:number, index2:number):void {
+        [this.data[index1], this.data[index2]] = [this.data[index2], this.data[index1]];
+    }
+
+    public size():number {
+        return this.data.length;
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
+
 export {
     characterReplacement,
     regionsBySlashes,

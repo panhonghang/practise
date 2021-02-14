94837cd5abb0d85bc7f1cff8b26a91c22fba2c6c
pangonghang <1966656576@qq.com>
2021-02-14 19:47:19 +0800

    情侣牵手

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -3990,6 +3990,48 @@ function findDisappearedNumbers(nums: number[]): number[] {
     return ret;
 };
 
+function minSwapsCouples(row: number[]): number {
+    const n:number = row.length;
+    const tot:number = n / 2;
+    const f:number[] = new Array(tot).fill(0).map((element, index) => index);
+    
+    for (let i = 0; i < n; i += 2) {
+        const l:number = Math.floor(row[i] / 2);
+        const r:number = Math.floor(row[i + 1] / 2);
+        add(f, l, r);
+    }
+    const map:Map<number, number> = new Map();
+    for (let i = 0; i < tot; i++) {
+        const fx = getf(f, i);
+        if (map.has(fx)) {
+            map.set(fx, map.get(fx)! + 1);
+        } else {
+            map.set(fx, 1)
+        }
+    }
+    
+    let ret:number = 0;
+    for (const [f, sz] of map.entries()) {
+        ret += sz - 1;
+    }
+    return ret;
+};  
+
+const getf = (f:number[], x:number):number => {
+    if (f[x] === x) {
+        return x;
+    }
+    const newf:number = getf(f, f[x]);
+    f[x] = newf;
+    return newf;
+}
+
+const add = (f:number[], x:number, y:number):void => {
+    const fx:number = getf(f, x);
+    const fy:number = getf(f, y);
+    f[fx] = fy;
+};
+
 export {
     characterReplacement,
     regionsBySlashes,

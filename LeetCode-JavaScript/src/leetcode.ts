85f80abede92cce00af336d087b623841f508b66
pangonghang <1966656576@qq.com>
2021-03-02 12:44:01 +0800

    二维区域和检索 - 矩阵不可变

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -4300,6 +4300,29 @@ class NumArray {
  * var param_1 = obj.sumRange(i,j)
  */
 
+class NumMatrix {
+    private matrix:number[][];
+    constructor(matrix: number[][]) {
+        this.matrix = matrix;
+    }
+
+    public sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+        let sum:number = 0;
+        for (let i = row1; i <= row2; i++) {
+            for (let j = col1; j <= col2; j++) {
+                sum += this.matrix[i][j]
+            }
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
+ 
 export {
     characterReplacement,
     regionsBySlashes,

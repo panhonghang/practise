129bd15f7561b281061a593ba5ebd0e1cd8d7ab5
pangonghang <1966656576@qq.com>
2021-03-07 17:24:08 +0800

    分割字符串II java版本

--- LeetCode-Java/TreeNode.java
+++ LeetCode-Java/TreeNode.java
@@ -866,3 +866,35 @@ import java.util.stream.Stream;
 //         return s.length(); 
 //     }
 // }
+
+class Solution {
+		public List<List<String>> partition(String s) {
+				List<List<String>> res = new ArrayList<List<String>>();
+				List<String> list = new ArrayList<String>();
+				dfs(s, 0, list, res);
+				return res;
+		}
+		
+		public void dfs(String s, int pos, List<String> list, List<List<String>> res){
+				if (pos == s.length()) {
+						res.add(new ArrayList<String>(list));
+						return;
+				}
+				for (int i = pos; i < s.length(); i++) {
+						if (isPalindrome(s, pos, i)){
+								list.add(s.substring(pos, i+1));
+								dfs(s, i+1, list, res);
+								list.remove(list.size() - 1);
+						}
+				}
+		}
+
+		public boolean isPalindrome (String s, int start, int end) {
+				for (int i = start, j = end; i < j; i++, j--) {
+						if (s.charAt(i) != s.charAt(j)) {
+								return false;
+						}
+				}
+				return true;
+		}
+}

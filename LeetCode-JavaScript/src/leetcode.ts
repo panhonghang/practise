b350ff45d9aa59112cba647bc42e3d78cef33bc6
pangonghang <1966656576@qq.com>
2021-03-17 16:17:10 +0800

    不同的子序列

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -4740,7 +4740,25 @@ function generateMatrix(n: number): number[][] {
     r[i][j] = k;
     return r;
 };
-
+function numDistinct(s: string, t: string): number {
+    const m:number = s.length, 
+          n:number = t.length,
+          dp:number[][] = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+    if (m < n) return 0;
+    
+    for (let i = 0; i <= m; i++) dp[i][n] = 1;
+    
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            if (s[i] == t[j]) {
+                dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+            } else {
+                dp[i][j] = dp[i + 1][j];
+            }
+        }
+    }
+    return dp[0][0];
+};
 export {
     calculate,
     characterReplacement,

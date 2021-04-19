3c12ca2489e8aa748c24dfda3ccae140b30f0bf2
pangonghang <1966656576@qq.com>
2021-04-20 00:28:25 +0800

    kmp

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -5455,6 +5455,26 @@ function strStr(haystack: string, needle: string): number {
     return -1;
 };
 
+function strStrII(haystack: string, needle: string): number {
+    const n: number = haystack.length, 
+          m: number = needle.length;
+    if (m === 0) return 0;
+    const pi: number[] = new Array(m).fill(0);
+
+    for (let i = 1, j = 0; i < m; i++) {
+        while (j > 0 && needle[i] !== needle[j]) j = pi[j - 1];
+        if (needle[i] == needle[j]) j++;
+        pi[i] = j;
+    }
+
+    for (let i = 0, j = 0; i < n; i++) {
+        while (j > 0 && haystack[i] != needle[j]) j = pi[j - 1];
+        if (haystack[i] == needle[j]) j++;
+        if (j === m) return i - m + 1;
+    }
+    return -1;
+};
+
 export {
     removeDuplicatesII,
     isScramble,

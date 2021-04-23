4bce5b1bd3b53bfcc6ba69bf9dc93f64098b80bd
pangonghang <1966656576@qq.com>
2021-04-24 01:19:13 +0800

    组合总和 Ⅳ

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -5585,6 +5585,18 @@ function largestDivisibleSubset(nums: number[]): number[] {
     }
     return res;
 };
+function combinationSum4(nums: number[], target: number): number {
+    const dp: number[] = new Array(target + 1).fill(0);
+    dp[0] = 1;
+    for (let i = 1; i <= target; i++) {
+        for (const num of nums) {
+            if (num <= i) {
+                dp[i] += dp[i - num];
+            }
+        }
+    }
+    return dp[target];
+};
 export {
     removeDuplicatesII,
     isScramble,

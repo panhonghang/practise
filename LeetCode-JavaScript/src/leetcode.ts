d681ddafd8556d60cef44fc9f2167c7673a13349
pangonghang <1966656576@qq.com>
2021-04-29 23:28:26 +0800

    青蛙跳石头

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -5663,6 +5663,30 @@ function judgeSquareSum(c: number): boolean {
     return false;
 };
 
+function canCross(stones: number[]): boolean {
+    const n: number = stones.length;
+    const dp: boolean[][] = new Array(n).fill(0).map(() => new Array(n).fill(false));
+    dp[0][0] = true;
+    for (let i = 1; i < n; ++i) {
+        if (stones[i] - stones[i - 1] > i) {
+            return false;
+        }
+    }
+    for (let i = 1; i < n; ++i) {
+        for (let j = i - 1; j >= 0; --j) {
+            const k = stones[i] - stones[j];
+            if (k > j + 1) {
+                break;
+            }
+            dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+            if (i === n - 1 && dp[i][k]) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
 export {
     removeDuplicatesII,
     isScramble,

27efb5411949888a7661e0be50afe2e7d0b8355e
pangonghang <1966656576@qq.com>
2021-05-02 12:30:11 +0800

    砖墙

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -5722,6 +5722,23 @@ function GetImportance(employees: Employee[], id: number): number {
     return dfs(id);
 };
 
+function leastBricks(wall: number[][]): number {
+    const cnt: Map<number, number> = new Map();
+    for (const widths of wall) {
+        const n: number = widths.length;
+        let sum: number = 0;
+        for (let i = 0; i < n - 1; i++) {
+            sum += widths[i];
+            cnt.set(sum, (cnt.get(sum) || 0) + 1);
+        }
+    }
+    let maxCnt: number = 0;
+    for (const [_, c] of cnt.entries()) {
+        maxCnt = Math.max(maxCnt, c);
+    }
+    return wall.length - maxCnt;
+};
+
 export {
     removeDuplicatesII,
     isScramble,

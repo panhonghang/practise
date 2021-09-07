edea7cff0c5055e431724dda696dcc3c4d1eb40b
pangonghang <1966656576@qq.com>
2021-09-07 23:11:28 +0800

    平衡字符串

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -6088,6 +6088,18 @@ function search(nums: number[], target: number): number {
     
     return -1;
 };
+
+function balancedStringSplit(s: string): number {
+  //L is 1 R is -1
+  let sum: number = 0;
+  let res: number = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'L') sum++;
+    if (s[i] === 'R') sum--;
+    if (sum === 0) res++;
+  }
+  return res;
+};
 export {
     removeDuplicatesII,
     isScramble,

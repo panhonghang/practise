a190ee9a00655a04e301d6007ef8d5a07251faf7
pangonghang <1966656576@qq.com>
2021-09-15 21:24:34 +0800

    爬坡

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -6355,6 +6355,19 @@ function findLongestWord(s: string, dictionary: string[]): string {
     }
     return '';
 };
+function findPeakElement(nums: number[]): number {
+    let left = 0, 
+    right = nums.length - 1;
+    while (left < right ) {
+        let mid = left + Math.floor((right - left) / 2);
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+};
 export {
     removeDuplicatesII,
     isScramble,

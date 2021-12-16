f5bf7670326835a2ed7977023a04fa0196ff6076
pangonghang <1966656576@qq.com>
2021-12-16 21:35:29 +0800

    1610. 可见点的最大数目

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -6474,7 +6474,39 @@ function loudAndRich(richer: number[][], quiet: number[]): number[] {
     }
     return ans
 };
+function visiblePoints(points: number[][], angle: number, location: number[]): number {
+    const calc = (x,y) => {
+        return Math.atan2(y- location[1],x - location[0]) * 180 / Math.PI;
+    }
 
+    let self = 0
+    let ret = 0
+    let angles = []
+
+    for(const [x,y] of points){
+        if(x === location[0] && y === location[1]){
+            self++
+        }else{
+            const rad = calc(x,y)
+            angles.push(rad)
+        }
+    }
+
+    angles.sort((a,b) => a - b)
+    // console.log(angles)
+    angles = angles.concat(angles.map((a) => a + 360))
+    // console.log(angles)
+
+
+    for(let l = 0, r = 0; r < (angles.length); r++){
+        while(l < r && angles[r] - angles[l] > angle){
+            l++
+        }
+        ret = Math.max(ret, r - l + 1)
+    }
+
+    return ret + self;
+};
 export {
     removeDuplicatesII,
     isScramble,

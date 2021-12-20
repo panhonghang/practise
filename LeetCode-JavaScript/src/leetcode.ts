0bd7ccb84db88c07ec0e74258815ff2f51d9b2cc
pangonghang <1966656576@qq.com>
2021-12-20 23:14:30 +0800

    475. 供暖器

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -6546,6 +6546,22 @@ function findJudge(n: number, trust: number[][]): number {
 	}
 	return -1;
 };
+function findRadius(houses: number[], heaters: number[]): number {
+    houses.sort((a,b)=>a-b)
+    heaters.sort((a,b)=>a-b)
+    let ans = 0
+    for(let i=0, j=0; i < houses.length; i++){
+        let cur = Math.abs(houses[i] - heaters[j])
+        while(j < heaters.length && heaters[j] <= houses[i])
+            cur = houses[i] - heaters[j++]
+        if(j < heaters.length)
+            cur = Math.min(cur, heaters[j] - houses[i])
+        ans = Math.max(ans, cur)
+        if(j > 0)
+            j--
+    }
+    return ans
+};
 export {
     removeDuplicatesII,
     isScramble,

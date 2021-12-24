b3cc704d3ef7ab4211fe65c5edfffb4f738873ec
pangonghang <1966656576@qq.com>
2021-12-24 22:46:09 +0800

    吃苹果

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -6608,6 +6608,20 @@ function longestDupSubstring(s: string): string {
 
     return str;
 };
+function eatenApples(apples: number[], days: number[]): number {
+    let res = 0, minDay = days[0] - 1;
+    const freshArr = new Array(days.length).fill(0);
+    for (let i = 0; i < freshArr.length; i++) {
+        if (i < days.length && days[i] > 0) {
+            freshArr[i + days[i] - 1] = (freshArr[i + days[i] - 1] || 0) + apples[i];
+            minDay = Math.min(minDay, i + days[i] - 1);
+        }
+        minDay = Math.max(minDay, i);
+        while (minDay < freshArr.length && !freshArr[minDay]) minDay++;
+        if (freshArr[minDay]) res++, freshArr[minDay]--;
+    }
+    return res;
+};
 export {
     removeDuplicatesII,
     isScramble,

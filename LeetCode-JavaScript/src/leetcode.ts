d432531ee93d1efd3c86937f14e3805e842d0c89
pangonghang <1966656576@qq.com>
2022-01-14 01:04:12 +0800

    373. 查找和最小的K对数字

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -7169,6 +7169,101 @@ function swap<T = number>(arr: T[], a: number, b: number) {
   
     return result
   };
+  type HeapType = "min" | "max";
+  class Heap<T = number> {
+      type: HeapType;
+      heap: T[];
+      constructor(type: HeapType = "min", compare?: (a: T, b: T) => boolean) {
+          this.type = type;
+          this.compare = compare || this.compare;
+          this.heap = [];
+      }
+      compare(a: T, b: T) {
+          return this.type === "min" ? a < b : a > b;
+      }
+      isEqual(a: T, b: T) {
+          return a === b;
+      }
+      swap(i: number, j: number) {
+          [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+      }
+      insert(val: T) {
+          this.heap.push(val);
+          this.shiftUp(this.heap.length - 1);
+      }
+      getParentIndex(index: number) {
+          return Math.floor((index - 1) / 2);
+      }
+      getLeftIndex(index: number) {
+          return 2 * index + 1;
+      }
+      getRightIndex(index: number) {
+          return 2 * index + 2;
+      }
+      shiftUp(index: number) {
+          if (index === 0) return;
+          const parentIndex = this.getParentIndex(index);
+          if (
+              this.heap[parentIndex] != null &&
+              this.compare(this.heap[index], this.heap[parentIndex])
+          ) {
+              this.swap(parentIndex, index);
+              this.shiftUp(parentIndex);
+          }
+      }
+      pop() {
+          if (this.heap.length === 0) throw new Error("error");
+          if (this.heap.length === 1) return this.heap.pop()!;
+          const res = this.heap[0];
+          this.heap[0] = this.heap.pop()!;
+          this.shiftDown(0);
+          return res;
+      }
+      shiftDown(index: number) {
+          const leftIndex = this.getLeftIndex(index);
+          const rightIndex = this.getRightIndex(index);
+          if (
+              this.heap[leftIndex] != null &&
+              this.compare(this.heap[leftIndex], this.heap[index])
+          ) {
+              this.swap(leftIndex, index);
+              this.shiftDown(leftIndex);
+          }
+          if (
+              this.heap[rightIndex] != null &&
+              this.compare(this.heap[rightIndex], this.heap[index])
+          ) {
+              this.swap(rightIndex, index);
+              this.shiftDown(rightIndex);
+          }
+      }
+      peek() {
+          if (this.heap.length === 0) throw new Error("error");
+          return this.heap[0];
+      }
+      size() {
+          return this.heap.length;
+      }
+  }
+  
+  function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][] {
+      const heap = new Heap<[number, number]>(
+          "min",
+          (a, b) => nums1[a[0]] + nums2[a[1]] < nums1[b[0]] + nums2[b[1]]
+      );
+      const res: [number, number][] = [];
+      const set = new Set<string>()
+      heap.insert([0, 0]);
+      while (heap.size() > 0 && res.length < k) {
+          const arr = heap.pop();
+          if(set.has(arr.join('.'))) continue
+          res.push([nums1[arr[0]], nums2[arr[1]]]);
+          set.add(arr.join('.'))
+          if (arr[0] + 1 < nums1.length) heap.insert([arr[0] + 1, arr[1]]);
+          if (arr[1] + 1 < nums2.length) heap.insert([arr[0], arr[1] + 1]);
+      }
+      return res;
+  };
 export {
     removeDuplicatesII,
     isScramble,

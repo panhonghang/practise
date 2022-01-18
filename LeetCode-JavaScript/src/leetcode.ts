c6f5fe5cba71b69c8d944de8c82171ccd98a3e5b
pangonghang <1966656576@qq.com>
2022-01-19 00:10:10 +0800

    219. 存在重复元素 II

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -7439,7 +7439,14 @@ function containsNearbyDuplicate(nums: number[], k: number): boolean {
     }
     return false;
 };
-
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    const map: Map<number, number> = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i]) && i - map.get(nums[i]) <= k) return true;
+        map.set(nums[i], i);
+    }
+    return false;
+};
 export {
     removeDuplicatesII,
     isScramble,

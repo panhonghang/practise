6027afcce006a2d6443ffb8e79909e39dd074dbf
pangonghang <1966656576@qq.com>
2022-01-21 01:06:54 +0800

    1345. 跳跃游戏 IV

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -7640,6 +7640,64 @@ function smallestK(arr: number[], k: number): number[] {
     }
     return ans;
 };
+function minJumps(arr: number[]): number {
+    if (arr.length === 1) return 0;
+  
+    const len: number = arr.length;
+    const map: Map<number, number[]> = new Map();
+  
+    for (let i = 0; i < len; i++) {
+      if (!map.has(arr[i])) map.set(arr[i], []);
+      map.get(arr[i]).push(i)
+    }
+   
+    let beginSet: Set<number> = new Set([0])
+    let endSet: Set<number> = new Set([len - 1])
+  
+    let level = 1
+    //存储所有出现的index，避免重复使用，剪枝
+    const all = new Set([...beginSet, ...endSet])
+  
+    while (beginSet.size > 0) {
+      //存储beginSet能到到所有index
+      const newSet: Set<number> = new Set()
+      for (const item of beginSet) {
+        if (endSet.has(item)) {
+          return level
+        }
+        //遍历arr[item]能到到所有index
+        for (const index of map.get(arr[item])) {
+          if (endSet.has(index)) {
+            return level
+          }
+          if (all.has(index)) continue
+          newSet.add(index)
+          all.add(index)
+        }
+        const add = item + 1
+        const del = item - 1
+  
+        if (endSet.has(add) || endSet.has(del)) {
+          return level
+        }
+        if (add < len && !all.has(add)) {
+          newSet.add(add)
+          all.add(add)
+        }
+        if (del >= 0 && !all.has(del)) {
+          newSet.add(del)
+          all.add(del)
+        }
+      }
+      beginSet = newSet
+      level++
+      //每次使用较少的index集合
+      if (beginSet.size > endSet.size) {
+        [beginSet, endSet] = [endSet, beginSet]
+      }
+    }
+    return 0
+  };
 export {
     removeDuplicatesII,
     isScramble,

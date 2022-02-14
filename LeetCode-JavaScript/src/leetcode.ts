dd8535e85f9832ff68db49cae8a11a8e3c547d14
pangonghang <1966656576@qq.com>
2022-02-15 00:55:40 +0800

    1380. 矩阵中的幸运数

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -8331,6 +8331,27 @@ function highestPeak(isWater: number[][]): number[][] {
     return dp;
 };
 
+function luckyNumbers (matrix: number[][]): number[] {
+    const m = matrix.length, n = matrix[0].length;
+    const minRow = new Array(m).fill(Number.MAX_SAFE_INTEGER);
+    const maxCol = new Array(n).fill(0);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            minRow[i] = Math.min(minRow[i], matrix[i][j]);
+            maxCol[j] = Math.max(maxCol[j], matrix[i][j]);
+        }
+    }
+    const ret = [];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === minRow[i] && matrix[i][j] === maxCol[j]) {
+                ret.push(matrix[i][j]);
+            }
+        }
+    }
+    return ret;
+};
+
 export {
     removeDuplicatesII,
     isScramble,

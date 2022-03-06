be7a7bd336744202fc612744943438c217634e58
pangonghang <1966656576@qq.com>
2022-03-06 21:07:48 +0800

    2100. 适合打劫银行的日子

--- LeetCode-JavaScript/src/leetcode.ts
+++ LeetCode-JavaScript/src/leetcode.ts
@@ -8754,6 +8754,27 @@ function findLUSlength(a: string, b: string): number {
     if (a == b) return -1
     return Math.max(a.length, b.length);
 };
+function goodDaysToRobBank(security: number[], time: number): number[] {
+    const n: number = security.length;
+    const left: number[] = new Array(n).fill(0);
+    const right: number[] = new Array(n).fill(0);
+    for (let i = 1; i < n; i++) {
+        if (security[i] <= security[i - 1]) {
+            left[i] = left[i - 1] + 1;
+        }
+        if (security[n - i - 1] <= security[n - i]) {
+            right[n - i - 1] = right[n - i] + 1;
+        }
+    }
+
+    const ans: number[] = [];
+    for (let i = time; i < n - time; i++) {
+        if (left[i] >= time && right[i] >= time) {
+            ans.push(i);    
+        }
+    }
+    return ans;
+};
 export {
     removeDuplicatesII,
     isScramble,
